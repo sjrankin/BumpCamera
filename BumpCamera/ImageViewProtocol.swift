78d1938c2846cc51d4196a0ddabda653dd908c05
Stuart Rankin <sjrankin@gmail.com>
2019-01-10 22:05:15 +0900

    Initial commit. Protocol for image preview view controller.

---
+++ BumpCamera/ImageViewProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  ImageViewProtocol.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 1/10/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol ImageViewProtocol
+{
+    func ImageToPreview(_ Image: UIImage)
+}
