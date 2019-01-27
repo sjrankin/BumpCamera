b65c98ac1e838f4021051b6d377fc8de8eae01ae
Stuart Rankin <Stuart@Norio4.local>
2019-01-27 22:49:00 +0900

    Create NewFieldSettingProtocol.swift
    Initial check-in. Protocol for filter storyboards to communicate with the filter setting view controller.

---
+++ BumpCamera/SettingsUI/NewFieldSettingProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  NewFieldSettingProtocol.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 1/23/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol NewFieldSettingProtocol: class
+{
+    func NewRawValue()
+}
