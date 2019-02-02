72407d6d21bb8152b43fea4cd32f16c409a35dd5
Stuart Rankin <Stuart@Norio4.local>
2019-02-02 18:31:07 +0900

    Create Pixellate_Metal.swift
    Runs the pixellate Metal kernel.

---
+++ BumpCamera/Filters/Filters/Pixellate_Metal.swift
@@ -0,0 +1,390 @@
+//
+//  Pixellate_Metal.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 1/28/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreMedia
+import CoreVideo
+import MetalKit
+
+class Pixellate_Metal: FilterParent, Renderer
+{
+    required override init()
+    {
+        let DefaultLibrary = MetalDevice?.makeDefaultLibrary()
+        let KernelFunction = DefaultLibrary?.makeFunction(name: "PixellateKernel")
+        do
+        {
+            ComputePipelineState = try MetalDevice?.makeComputePipelineState(function: KernelFunction!)
+        }
+        catch
+        {
+            print("Unable to create pipeline state: \(error.localizedDescription)")
+        }
+    }
+    
+    var _ID: UUID = UUID(uuidString: "ea2602d1-468e-4ff4-a1ea-1299af4b70aa")!
+    var ID: UUID
+    {
+        get
+        {
+            return _ID
+        }
+        set
+        {
+            _ID = newValue
+        }
+    }
+    
+    var InstanceID: UUID
+    {
+        return UUID()
+    }
+    
+    var Description: String = "Metal Pixellate"
+    
+    var IconName: String = "Metal Pixellate"
+    
+    private let MetalDevice = MTLCreateSystemDefaultDevice()
+    
+    private var ComputePipelineState: MTLComputePipelineState? = nil
+    
+    private lazy var CommandQueue: MTLCommandQueue? =
+    {
+        return self.MetalDevice?.makeCommandQueue()
+    }()
+    
+    private(set) var OutputFormatDescription: CMFormatDescription? = nil
+    
+    private(set) var InputFormatDescription: CMFormatDescription? = nil
+    
+    private var BufferPool: CVPixelBufferPool? = nil
+    
+    var Initialized = false
+    
+    func Initialize(With FormatDescription: CMFormatDescription, BufferCountHint: Int)
+    {
+        Reset("Pixellate_Metal.Initialize")
+        (BufferPool, _, OutputFormatDescription) = CreateBufferPool(From: FormatDescription, BufferCountHint: BufferCountHint)
+        if BufferPool == nil
+        {
+            print("BufferPool nil in Pixellate_Metal.Initialize.")
+            return
+        }
+        InputFormatDescription = FormatDescription
+        
+        Initialized = true
+        
+        var MetalTextureCache: CVMetalTextureCache? = nil
+        if CVMetalTextureCacheCreate(kCFAllocatorDefault, nil, MetalDevice!, nil, &MetalTextureCache) != kCVReturnSuccess
+        {
+            fatalError("Unable to allocation texture cache in Pixellate_Metal.")
+        }
+        else
+        {
+            TextureCache = MetalTextureCache
+        }
+    }
+    
+    func Reset(_ CalledBy: String = "")
+    {
+        objc_sync_enter(AccessLock)
+        defer{objc_sync_exit(AccessLock)}
+        BufferPool = nil
+        OutputFormatDescription = nil
+        InputFormatDescription = nil
+        TextureCache = nil
+        Initialized = false
+    }
+    
+    func Reset()
+    {
+        Reset("")
+    }
+    
+    var AccessLock = NSObject()
+    
+    var ParameterBuffer: MTLBuffer? = nil
+    
+    func Render(PixelBuffer: CVPixelBuffer) -> CVPixelBuffer?
+    {
+        if !Initialized
+        {
+            fatalError("Pixellate_Metal not initialized at Render(CVPixelBuffer) call.")
+        }
+        
+        var FinalWidth: Int = 20
+        if let RawWidth = ParameterManager.GetField(From: ID, Field: FilterManager.InputFields.BlockWidth)
+        {
+            if let DS = RawWidth as? Int
+            {
+                FinalWidth = DS
+            }
+        }
+        var FinalHeight: Int = 20
+        if let RawHeight = ParameterManager.GetField(From: ID, Field: FilterManager.InputFields.BlockHeight)
+        {
+            if let DS = RawHeight as? Int
+            {
+                FinalHeight = DS
+            }
+        }
+        let Buffer0 = BlockInfoParameters(Width: uint(FinalWidth), Height: uint(FinalHeight))
+        let Buffers = [Buffer0]
+        ParameterBuffer = MetalDevice!.makeBuffer(length: MemoryLayout<BlockInfoParameters>.size, options: [])
+        memcpy(ParameterBuffer?.contents(), Buffers, MemoryLayout<BlockInfoParameters>.size)
+        
+        var NewPixelBuffer: CVPixelBuffer? = nil
+        CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, BufferPool!, &NewPixelBuffer)
+        guard let OutputBuffer = NewPixelBuffer else
+        {
+            print("Allocation failure for new pixel buffer pool in Pixellate_Metal.")
+            return nil
+        }
+        
+        guard let InputTexture = MakeTextureFromCVPixelBuffer(pixelBuffer: PixelBuffer, textureFormat: .bgra8Unorm),
+            let OutputTexture = MakeTextureFromCVPixelBuffer(pixelBuffer: OutputBuffer, textureFormat: .bgra8Unorm) else
+        {
+            print("Error creating textures in Pixellate_Metal.")
+            return nil
+        }
+        
+        guard let CommandQ = CommandQueue,
+            let CommandBuffer = CommandQ.makeCommandBuffer(),
+            let CommandEncoder = CommandBuffer.makeComputeCommandEncoder() else
+        {
+            print("Error creating Metal command queue.")
+            CVMetalTextureCacheFlush(TextureCache!, 0)
+            return nil
+        }
+        
+        CommandEncoder.label = "Pixellate Metal"
+        CommandEncoder.setComputePipelineState(ComputePipelineState!)
+        CommandEncoder.setTexture(InputTexture, index: 0)
+        CommandEncoder.setTexture(OutputTexture, index: 1)
+        CommandEncoder.setBuffer(ParameterBuffer, offset: 0, index: 0)
+        
+        let w = ComputePipelineState!.threadExecutionWidth
+        let h = ComputePipelineState!.maxTotalThreadsPerThreadgroup / w
+        let ThreadsPerThreadGroup = MTLSize(width: w, height: h, depth: 1)
+        let ThreadGroupsPerGrid = MTLSize(width: (InputTexture.width + w - 1) / w,
+                                          height: (InputTexture.height + h - 1) / h,
+                                          depth: 1)
+        CommandEncoder.dispatchThreadgroups(ThreadGroupsPerGrid, threadsPerThreadgroup: ThreadsPerThreadGroup)
+        CommandEncoder.endEncoding()
+        CommandBuffer.commit()
+        
+        return OutputBuffer
+    }
+    
+    var ImageDevice: MTLDevice? = nil
+    var InitializedForImage = false
+    private var ImageComputePipelineState: MTLComputePipelineState? = nil
+    private lazy var ImageCommandQueue: MTLCommandQueue? =
+    {
+        return self.MetalDevice?.makeCommandQueue()
+    }()
+    
+    func InitializeForImage()
+    {
+        ImageDevice = MTLCreateSystemDefaultDevice()
+        let DefaultLibrary = ImageDevice?.makeDefaultLibrary()
+        let KernelFunction = DefaultLibrary?.makeFunction(name: "PixellateKernel")
+        do
+        {
+            ImageComputePipelineState = try MetalDevice!.makeComputePipelineState(function: KernelFunction!)
+        }
+        catch
+        {
+            print("Unable to create pipeline state: \(error.localizedDescription)")
+        }
+        
+        InitializedForImage = true
+    }
+    
+        var ImageParameterBuffer: MTLBuffer? = nil
+    
+    //http://flexmonkey.blogspot.com/2014/10/metal-kernel-functions-compute-shaders.html
+    func Render(Image: UIImage) -> UIImage?
+    {
+        if !InitializedForImage
+        {
+            fatalError("Not initialized.")
+        }
+        let CgImage = Image.cgImage
+        let ImageWidth: Int = (CgImage?.width)!
+        let ImageHeight: Int = (CgImage?.height)!
+        var RawData = [UInt8](repeating: 0, count: Int(ImageWidth * ImageHeight * 4))
+        let RGBColorSpace = CGColorSpaceCreateDeviceRGB()
+        let BitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue)
+        let Context = CGContext(data: &RawData, width: ImageWidth, height: ImageHeight, bitsPerComponent: (CgImage?.bitsPerComponent)!,
+                                bytesPerRow: (CgImage?.bytesPerRow)!, space: RGBColorSpace, bitmapInfo: BitmapInfo.rawValue)
+        Context!.draw(CgImage!, in: CGRect(x: 0, y: 0, width: CGFloat(ImageWidth), height: CGFloat(ImageHeight)))
+        let TextureDescriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: .rgba8Unorm,
+                                                                         width: Int(ImageWidth), height: Int(ImageHeight), mipmapped: true)
+        guard let Texture = ImageDevice?.makeTexture(descriptor: TextureDescriptor) else
+        {
+            print("Error creating input texture in Pixellate_Metal.Render.")
+            return nil
+        }
+        
+        let Region = MTLRegionMake2D(0, 0, Int(ImageWidth), Int(ImageHeight))
+        Texture.replace(region: Region, mipmapLevel: 0, withBytes: &RawData, bytesPerRow: Int((CgImage?.bytesPerRow)!))
+        let OutputTextureDescriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: Texture.pixelFormat,
+                                                                               width: Texture.width, height: Texture.height, mipmapped: true)
+        let OutputTexture = ImageDevice?.makeTexture(descriptor: OutputTextureDescriptor)
+        
+        let CommandBuffer = ImageCommandQueue?.makeCommandBuffer()
+        let CommandEncoder = CommandBuffer?.makeComputeCommandEncoder()
+        
+        CommandEncoder?.setComputePipelineState(ImageComputePipelineState!)
+        CommandEncoder?.setTexture(Texture, index: 0)
+        CommandEncoder?.setTexture(OutputTexture, index: 1)
+        
+        var FinalWidth: Int = 20
+        if let RawWidth = ParameterManager.GetField(From: ID, Field: .BlockWidth)
+        {
+            if let DS = RawWidth as? Int
+            {
+                FinalWidth = DS
+            }
+        }
+        var FinalHeight: Int = 20
+        if let RawHeight = ParameterManager.GetField(From: ID, Field: .BlockHeight)
+        {
+            if let DS = RawHeight as? Int
+            {
+                FinalHeight = DS
+            }
+        }
+        print("Block height in Pixellate_Metal: \(FinalWidth)x\(FinalHeight)")
+        let Buffer0 = BlockInfoParameters(Width: uint(FinalWidth), Height: uint(FinalHeight))
+        let Buffers = [Buffer0]
+        ImageParameterBuffer = MetalDevice!.makeBuffer(length: MemoryLayout<BlockInfoParameters>.size, options: [])
+        memcpy(ImageParameterBuffer?.contents(), Buffers, MemoryLayout<BlockInfoParameters>.size)
+        CommandEncoder!.setBuffer(ImageParameterBuffer, offset: 0, index: 0)
+        
+        let ThreadGroupCount  = MTLSizeMake(8, 8, 1)
+        let ThreadGroups = MTLSizeMake(Texture.width / ThreadGroupCount.width,
+                                       Texture.height / ThreadGroupCount.height,
+                                       1)
+        
+        ImageCommandQueue = ImageDevice?.makeCommandQueue()
+        
+        CommandEncoder!.dispatchThreadgroups(ThreadGroups, threadsPerThreadgroup: ThreadGroupCount)
+        CommandEncoder!.endEncoding()
+        CommandBuffer?.commit()
+        CommandBuffer?.waitUntilCompleted()
+        
+        let ImageSize = CGSize(width: Texture.width, height: Texture.height)
+        let ImageByteCount = Int(ImageSize.width * ImageSize.height * 4)
+        let BytesPerRow = CgImage?.bytesPerRow
+        var ImageBytes = [UInt8](repeating: 0, count: ImageByteCount)
+        let ORegion = MTLRegionMake2D(0, 0, Int(ImageSize.width), Int(ImageSize.height))
+        OutputTexture?.getBytes(&ImageBytes, bytesPerRow: BytesPerRow!, from: ORegion, mipmapLevel: 0)
+        
+        let SizeOfUInt8 = UInt8.SizeOf()
+        let Provider = CGDataProvider(data: NSData(bytes: &ImageBytes, length: ImageBytes.count * SizeOfUInt8))
+        let OBitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue)
+        let RenderingIntent = CGColorRenderingIntent.defaultIntent
+        let FinalImage = CGImage(width: Int(ImageSize.width), height: Int(ImageSize.height),
+                                 bitsPerComponent: (CgImage?.bitsPerComponent)!, bitsPerPixel: (CgImage?.bitsPerPixel)!,
+                                 bytesPerRow: BytesPerRow!, space: RGBColorSpace, bitmapInfo: OBitmapInfo, provider: Provider!,
+                                 decode: nil, shouldInterpolate: false, intent: RenderingIntent)
+        return UIImage(cgImage: FinalImage!)
+    }
+    
+    func Render(Image: CIImage) -> CIImage?
+    {
+        let UImage = UIImage(ciImage: Image)
+        if let IFinal = Render(Image: UImage)
+        {
+            if let CFinal = IFinal.ciImage
+            {
+                return CFinal
+            }
+            else
+            {
+                print("Error converting UIImage to CIImage in Pixellate_Metal.Render(CIImage)")
+                return nil
+            }
+        }
+        else
+        {
+            print("Error returned from Render(UIImage) in Pixellate_Metal.Render(CIImage)")
+            return nil
+        }
+    }
+    
+
+    
+    func DefaultFieldValue(Field: FilterManager.InputFields) -> (FilterManager.InputTypes, Any?)
+    {
+        switch Field
+        {
+        case .BlockWidth:
+            return (.IntType, 20 as Any?)
+            
+        case .BlockHeight:
+            return (.IntType, 20 as Any?)
+            
+        case .HighlightColor:
+            return (.BoolType, false as Any?)
+            
+        case .HighlightSaturation:
+            return (.BoolType, false as Any?)
+            
+        case .HighlightBrightness:
+            return (.BoolType, false as Any?)
+            
+        case .MergeWithBackground:
+            return (.BoolType, false as Any?)
+            
+        default:
+            break
+        }
+        return (FilterManager.InputTypes.NoType, nil)
+    }
+    
+    func SupportedFields() -> [FilterManager.InputFields]
+    {
+        return Pixellate_Metal.SupportedFields()
+    }
+    
+    public static func SupportedFields() -> [FilterManager.InputFields]
+    {
+        var Fields = [FilterManager.InputFields]()
+        Fields.append(.BlockWidth)
+        Fields.append(.BlockHeight)
+        Fields.append(.HighlightColor)
+        Fields.append(.HighlightSaturation)
+        Fields.append(.HighlightBrightness)
+        Fields.append(.MergeWithBackground)
+        return Fields
+    }
+    
+    func SettingsStoryboard() -> String?
+    {
+        return Pixellate_Metal.SettingsStoryboard()
+    }
+    
+    public static func SettingsStoryboard() -> String?
+    {
+        return "Pixellate2Table"
+    }
+    
+    func IsSlow() -> Bool
+    {
+        return true
+    }
+    
+    func FilterTarget() -> [FilterTargets]
+    {
+        return [.LiveView, .Video, .Still]
+    }
+}
