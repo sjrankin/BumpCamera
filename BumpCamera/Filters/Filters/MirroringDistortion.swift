f407181d3d73c12f408a008b617bafe523528c31
Stuart Rankin <Stuart@Norio4.local>
2019-02-02 18:32:23 +0900

    Create MirroringDistortion.swift
    Code to run the mirroring kernel.

---
+++ BumpCamera/Filters/Filters/MirroringDistortion.swift
@@ -0,0 +1,355 @@
+//
+//  MirroringDistortion.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/2/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import UIKit
+import CoreMedia
+import CoreVideo
+import MetalKit
+
+class MirroringDistortion: FilterParent, Renderer
+{
+    required override init()
+    {
+        let DefaultLibrary = MetalDevice?.makeDefaultLibrary()
+        let KernelFunction = DefaultLibrary?.makeFunction(name: "MirroringKernel")
+        do
+        {
+            ComputePipelineState = try MetalDevice?.makeComputePipelineState(function: KernelFunction!)
+        }
+        catch
+        {
+            print("Unable to create pipeline state: \(error.localizedDescription)")
+        }
+    }
+    
+    var _ID: UUID = UUID(uuidString: "895f46c2-443d-4ffb-a3c3-bd3dacaf33b2")!
+    var ID: UUID
+    {
+        get
+        {
+            return _ID
+        }
+        set
+        {
+            _ID = newValue
+        }
+    }
+    
+    var InstanceID: UUID
+    {
+        return UUID()
+    }
+    
+    var Description: String = "Reflection"
+    
+    var IconName: String = "Reflection"
+    
+    private let MetalDevice = MTLCreateSystemDefaultDevice()
+    
+    private var ComputePipelineState: MTLComputePipelineState? = nil
+    
+    private lazy var CommandQueue: MTLCommandQueue? =
+    {
+        return self.MetalDevice?.makeCommandQueue()
+    }()
+    
+    private(set) var OutputFormatDescription: CMFormatDescription? = nil
+    
+    private(set) var InputFormatDescription: CMFormatDescription? = nil
+    
+    private var BufferPool: CVPixelBufferPool? = nil
+    
+    var Initialized = false
+    
+    func Initialize(With FormatDescription: CMFormatDescription, BufferCountHint: Int)
+    {
+        Reset("MirrorDistortion.Initialize")
+        (BufferPool, _, OutputFormatDescription) = CreateBufferPool(From: FormatDescription, BufferCountHint: BufferCountHint)
+        if BufferPool == nil
+        {
+            print("BufferPool nil in MirrorDistortion.Initialize.")
+            return
+        }
+        InputFormatDescription = FormatDescription
+        
+        Initialized = true
+        
+        var MetalTextureCache: CVMetalTextureCache? = nil
+        if CVMetalTextureCacheCreate(kCFAllocatorDefault, nil, MetalDevice!, nil, &MetalTextureCache) != kCVReturnSuccess
+        {
+            fatalError("Unable to allocation texture cache in MirrorDistortion.")
+        }
+        else
+        {
+            TextureCache = MetalTextureCache
+        }
+    }
+    
+    func Reset(_ CalledBy: String = "")
+    {
+        objc_sync_enter(AccessLock)
+        defer{objc_sync_exit(AccessLock)}
+        BufferPool = nil
+        OutputFormatDescription = nil
+        InputFormatDescription = nil
+        TextureCache = nil
+        Initialized = false
+    }
+    
+    func Reset()
+    {
+        Reset("")
+    }
+    
+    var AccessLock = NSObject()
+    
+    var ParameterBuffer: MTLBuffer! = nil
+    
+    func Render(PixelBuffer: CVPixelBuffer) -> CVPixelBuffer?
+    {
+        if !Initialized
+        {
+            fatalError("MirrorDistortion not initialized at Render(CVPixelBuffer) call.")
+        }
+        
+        let ImageWidth = CVPixelBufferGetWidth(PixelBuffer) / 2
+        let ImageHeight = CVPixelBufferGetHeight(PixelBuffer) / 2
+        let MDirection = ParameterManager.GetInt(From: ID, Field: .MirroringDirection, Default: 0)
+        let HSide = ParameterManager.GetInt(From: ID, Field: .HorizontalSide, Default: 0)
+        let VSide = ParameterManager.GetInt(From: ID, Field: .VerticalSide, Default: 0)
+        let Parameter = MirrorParameters(Direction: simd_uint1(MDirection), HorizontalSide: simd_uint1(HSide),
+                                         VerticalSide: simd_uint1(VSide), HorizontalAxis: simd_uint1(ImageWidth),
+                                         VerticalAxis: simd_uint1(ImageHeight))
+        let Parameters = [Parameter]
+        ParameterBuffer = MetalDevice!.makeBuffer(length: MemoryLayout<GrayscaleParameters>.size, options: [])
+        memcpy(ParameterBuffer.contents(), Parameters, MemoryLayout<GrayscaleParameters>.size)
+        
+        var NewPixelBuffer: CVPixelBuffer? = nil
+        CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, BufferPool!, &NewPixelBuffer)
+        guard let OutputBuffer = NewPixelBuffer else
+        {
+            print("Allocation failure for new pixel buffer pool in MirrorDistortion.")
+            return nil
+        }
+        
+        guard let InputTexture = MakeTextureFromCVPixelBuffer(pixelBuffer: PixelBuffer, textureFormat: .bgra8Unorm),
+            let OutputTexture = MakeTextureFromCVPixelBuffer(pixelBuffer: OutputBuffer, textureFormat: .bgra8Unorm) else
+        {
+            print("Error creating textures in MirrorDistortion.")
+            return nil
+        }
+        
+        guard let CommandQ = CommandQueue,
+            let CommandBuffer = CommandQ.makeCommandBuffer(),
+            let CommandEncoder = CommandBuffer.makeComputeCommandEncoder() else
+        {
+            print("Error creating Metal command queue.")
+            CVMetalTextureCacheFlush(TextureCache!, 0)
+            return nil
+        }
+        
+        CommandEncoder.label = "Mirroring Kernel"
+        CommandEncoder.setComputePipelineState(ComputePipelineState!)
+        CommandEncoder.setTexture(InputTexture, index: 0)
+        CommandEncoder.setTexture(OutputTexture, index: 1)
+        CommandEncoder.setBuffer(ParameterBuffer, offset: 0, index: 0)
+        
+        let w = ComputePipelineState!.threadExecutionWidth
+        let h = ComputePipelineState!.maxTotalThreadsPerThreadgroup / w
+        let ThreadsPerThreadGroup = MTLSize(width: w, height: h, depth: 1)
+        let ThreadGroupsPerGrid = MTLSize(width: (InputTexture.width + w - 1) / w,
+                                          height: (InputTexture.height + h - 1) / h,
+                                          depth: 1)
+        CommandEncoder.dispatchThreadgroups(ThreadGroupsPerGrid, threadsPerThreadgroup: ThreadsPerThreadGroup)
+        CommandEncoder.endEncoding()
+        CommandBuffer.commit()
+        
+        return OutputBuffer
+    }
+    
+    var ImageDevice: MTLDevice? = nil
+    var InitializedForImage = false
+    private var ImageComputePipelineState: MTLComputePipelineState? = nil
+    private lazy var ImageCommandQueue: MTLCommandQueue? =
+    {
+        return self.MetalDevice?.makeCommandQueue()
+    }()
+    
+    func InitializeForImage()
+    {
+        ImageDevice = MTLCreateSystemDefaultDevice()
+        let DefaultLibrary = ImageDevice?.makeDefaultLibrary()
+        let KernelFunction = DefaultLibrary?.makeFunction(name: "MirroringKernel")
+        do
+        {
+            ImageComputePipelineState = try MetalDevice?.makeComputePipelineState(function: KernelFunction!)
+        }
+        catch
+        {
+            print("Unable to create pipeline state: \(error.localizedDescription)")
+        }
+        
+        InitializedForImage = true
+    }
+    
+    //http://flexmonkey.blogspot.com/2014/10/metal-kernel-functions-compute-shaders.html
+    func Render(Image: UIImage) -> UIImage?
+    {
+        if !InitializedForImage
+        {
+            fatalError("Not initialized.")
+        }
+        let CgImage = Image.cgImage
+        let ImageWidth: Int = (CgImage?.width)!
+        let ImageHeight: Int = (CgImage?.height)!
+        var RawData = [UInt8](repeating: 0, count: Int(ImageWidth * ImageHeight * 4))
+        let RGBColorSpace = CGColorSpaceCreateDeviceRGB()
+        let BitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue)
+        let Context = CGContext(data: &RawData, width: ImageWidth, height: ImageHeight, bitsPerComponent: (CgImage?.bitsPerComponent)!,
+                                bytesPerRow: (CgImage?.bytesPerRow)!, space: RGBColorSpace, bitmapInfo: BitmapInfo.rawValue)
+        Context!.draw(CgImage!, in: CGRect(x: 0, y: 0, width: CGFloat(ImageWidth), height: CGFloat(ImageHeight)))
+        let TextureDescriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: .rgba8Unorm,
+                                                                         width: Int(ImageWidth), height: Int(ImageHeight), mipmapped: true)
+        guard let Texture = ImageDevice?.makeTexture(descriptor: TextureDescriptor) else
+        {
+            print("Error creating input texture in MirrorDistortion.Render.")
+            return nil
+        }
+        
+        let Region = MTLRegionMake2D(0, 0, Int(ImageWidth), Int(ImageHeight))
+        Texture.replace(region: Region, mipmapLevel: 0, withBytes: &RawData, bytesPerRow: Int((CgImage?.bytesPerRow)!))
+        let OutputTextureDescriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: Texture.pixelFormat,
+                                                                               width: Texture.width, height: Texture.height, mipmapped: true)
+        let OutputTexture = ImageDevice?.makeTexture(descriptor: OutputTextureDescriptor)
+        
+        let CommandBuffer = ImageCommandQueue?.makeCommandBuffer()
+        let CommandEncoder = CommandBuffer?.makeComputeCommandEncoder()
+        
+        CommandEncoder?.setComputePipelineState(ImageComputePipelineState!)
+        CommandEncoder?.setTexture(Texture, index: 0)
+        CommandEncoder?.setTexture(OutputTexture, index: 1)
+        
+        let ImageWidthHalf = ImageWidth / 2
+        let ImageHeightHalf = ImageHeight / 2
+        let MDirection = ParameterManager.GetInt(From: ID, Field: .MirroringDirection, Default: 0)
+        let HSide = ParameterManager.GetInt(From: ID, Field: .HorizontalSide, Default: 0)
+        let VSide = ParameterManager.GetInt(From: ID, Field: .VerticalSide, Default: 0)
+        let Parameter = MirrorParameters(Direction: simd_uint1(MDirection), HorizontalSide: simd_uint1(HSide),
+                                         VerticalSide: simd_uint1(VSide), HorizontalAxis: simd_uint1(ImageWidthHalf),
+                                         VerticalAxis: simd_uint1(ImageHeightHalf))
+        let Parameters = [Parameter]
+        ParameterBuffer = MetalDevice!.makeBuffer(length: MemoryLayout<GrayscaleParameters>.size, options: [])
+        memcpy(ParameterBuffer.contents(), Parameters, MemoryLayout<GrayscaleParameters>.size)
+        CommandEncoder!.setBuffer(ParameterBuffer, offset: 0, index: 0)
+        
+        let ThreadGroupCount  = MTLSizeMake(8, 8, 1)
+        let ThreadGroups = MTLSizeMake(Texture.width / ThreadGroupCount.width,
+                                       Texture.height / ThreadGroupCount.height,
+                                       1)
+        
+        ImageCommandQueue = ImageDevice?.makeCommandQueue()
+        
+        CommandEncoder!.dispatchThreadgroups(ThreadGroups, threadsPerThreadgroup: ThreadGroupCount)
+        CommandEncoder!.endEncoding()
+        CommandBuffer?.commit()
+        CommandBuffer?.waitUntilCompleted()
+        
+        let ImageSize = CGSize(width: Texture.width, height: Texture.height)
+        let ImageByteCount = Int(ImageSize.width * ImageSize.height * 4)
+        let BytesPerRow = CgImage?.bytesPerRow
+        var ImageBytes = [UInt8](repeating: 0, count: ImageByteCount)
+        let ORegion = MTLRegionMake2D(0, 0, Int(ImageSize.width), Int(ImageSize.height))
+        OutputTexture?.getBytes(&ImageBytes, bytesPerRow: BytesPerRow!, from: ORegion, mipmapLevel: 0)
+        
+        let SizeOfUInt8 = UInt8.SizeOf()
+        let Provider = CGDataProvider(data: NSData(bytes: &ImageBytes, length: ImageBytes.count * SizeOfUInt8))
+        let OBitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue)
+        let RenderingIntent = CGColorRenderingIntent.defaultIntent
+        let FinalImage = CGImage(width: Int(ImageSize.width), height: Int(ImageSize.height),
+                                 bitsPerComponent: (CgImage?.bitsPerComponent)!, bitsPerPixel: (CgImage?.bitsPerPixel)!,
+                                 bytesPerRow: BytesPerRow!, space: RGBColorSpace, bitmapInfo: OBitmapInfo, provider: Provider!,
+                                 decode: nil, shouldInterpolate: false, intent: RenderingIntent)
+        return UIImage(cgImage: FinalImage!)
+    }
+    
+    func Render(Image: CIImage) -> CIImage?
+    {
+        let UImage = UIImage(ciImage: Image)
+        if let IFinal = Render(Image: UImage)
+        {
+            if let CFinal = IFinal.ciImage
+            {
+                return CFinal
+            }
+            else
+            {
+                print("Error converting UIImage to CIImage in MirrorDistortion.Render(CIImage)")
+                return nil
+            }
+        }
+        else
+        {
+            print("Error returned from Render(UIImage) in MirrorDistortion.Render(CIImage)")
+            return nil
+        }
+    }
+    
+    func DefaultFieldValue(Field: FilterManager.InputFields) -> (FilterManager.InputTypes, Any?)
+    {
+        switch Field
+        {
+        case .MirroringDirection:
+            return (.IntType, 0 as Any?)
+            
+        case .HorizontalSide:
+            return (.IntType, 0 as Any?)
+            
+        case .VerticalSide:
+            return (.IntType, 0 as Any?)
+            
+        default:
+            return (.NoType, nil)
+        }
+    }
+    
+    func SupportedFields() -> [FilterManager.InputFields]
+    {
+        return GrayscaleAdjust.SupportedFields()
+    }
+    
+    public static func SupportedFields() -> [FilterManager.InputFields]
+    {
+        var Fields = [FilterManager.InputFields]()
+        Fields.append(.Command)
+        Fields.append(.MirroringDirection)
+        Fields.append(.HorizontalSide)
+        Fields.append(.VerticalSide)
+        Fields.append(.BAdjustment)
+        return Fields
+    }
+    
+    func SettingsStoryboard() -> String?
+    {
+        return GrayscaleAdjust.SettingsStoryboard()
+    }
+    
+    public static func SettingsStoryboard() -> String?
+    {
+        return "MirroringFilterSettingsUI"
+    }
+    
+    func IsSlow() -> Bool
+    {
+        return false
+    }
+    
+    func FilterTarget() -> [FilterTargets]
+    {
+        return [.LiveView, .Video, .Still]
+    }
+}
