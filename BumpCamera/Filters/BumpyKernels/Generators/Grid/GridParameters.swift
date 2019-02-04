c2a988788dc72abfbe007c309c7a489f01713336
Stuart Rankin <Stuart@Norio4.local>
2019-02-04 21:57:59 +0900

    Create GridParameters.swift
    Parameters for grid creation with the grid generator.

---
+++ BumpCamera/Filters/BumpyKernels/Generators/Grid/GridParameters.swift
@@ -0,0 +1,18 @@
+//
+//  GridParameters.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/4/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import simd
+
+struct GridParameters
+{
+    let GridX: simd_uint1
+    let GridY: simd_uint1
+    let GridColor: simd_float4
+    let BackgroundColor: simd_float4
+}
