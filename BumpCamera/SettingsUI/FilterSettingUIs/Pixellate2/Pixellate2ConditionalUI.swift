ff98458a33d5c0e99c2a96332adb32ba7ada131f
Stuart Rankin <Stuart@Norio4.local>
2019-02-11 22:56:13 +0900

    Create Pixellate2ConditionalUI.swift
    Placeholder code for code behind for conditional pixellation.

---
+++ BumpCamera/SettingsUI/FilterSettingUIs/Pixellate2/Pixellate2ConditionalUI.swift
@@ -0,0 +1,20 @@
+//
+//  Pixellate2ConditionalUI.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/11/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Pixellate2ConditionalUI: FilterSettingUIBase
+{
+    public var delegate: DescendentDelta? = nil
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
