d25fb5983dce021c512901fb67fbd9e6ae23fdae
Stuart Rankin <Stuart@Norio4.local>
2019-02-11 22:55:07 +0900

    Create SampleContainerView.swift
    Eventually may contain header view for filter views.

---
+++ BumpCamera/SettingsUI/FilterSettingUIs/SampleContainerView.swift
@@ -0,0 +1,15 @@
+//
+//  SampleContainerView.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/10/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SampleContainerView: UIView
+{
+    @IBOutlet weak var SampleImageView: UIImageView!
+}
