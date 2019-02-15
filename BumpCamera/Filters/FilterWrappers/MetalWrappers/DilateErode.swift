606441b3823dcf9d1a1dbbacba0396bbb1218c8e
Stuart Rankin <Stuart@Norio4.local>
2019-02-15 11:17:13 +0900

    Create DilateErode.swift
    Swift wrapper for calling the dilate/erode metal filter.

---
+++ BumpCamera/Filters/FilterWrappers/MetalWrappers/DilateErode.swift
@@ -0,0 +1,407 @@
+//
+//  DilateErode.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/14/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreMedia
+import CoreVideo
+import MetalKit
+
+class DilateErode: FilterParent, Renderer
+{
+    required override init()
+    {
+        let DefaultLibrary = MetalDevice?.makeDefaultLibrary()
+        let KernelFunction = DefaultLibrary?.makeFunction(name: "DilateErodeKernel")
+        do
+        {
+            ComputePipelineState = try MetalDevice?.makeComputePipelineState(function: KernelFunction!)
+        }
+        catch
+        {
+            print("Unable to create pipeline state: \(error.localizedDescription)")
+        }
+    }
+    
+    static let _ID: UUID = UUID(uuidString: "a068d790-76b2-4797-9a33-c58f7847db1a")!
+    
+    func ID() -> UUID
+    {
+        return DilateErode._ID
+    }
+    
+    static func ID() -> UUID
+    {
+        return _ID
+    }
+    
+    var InstanceID: UUID
+    {
+        return UUID()
+    }
+    
+    var Description: String = "Dilate and Erode"
+    
+    var IconName: String = "Dilate and Erode"
+    
+    private let MetalDevice = MTLCreateSystemDefaultDevice()
+    
+    private var ComputePipelineState: MTLComputePipelineState? = nil
+    
+    private lazy var CommandQueue: MTLCommandQueue? =
+    {
+        return self.MetalDevice?.makeCommandQueue()
+    }()
+    
+    private(set) var OutputFormatDescription: CMFormatDescription? = nil
+    
+    private(set) var InputFormatDescription: CMFormatDescription? = nil
+    
+    private var BufferPool: CVPixelBufferPool? = nil
+    
+    var Initialized = false
+    
+    func Initialize(With FormatDescription: CMFormatDescription, BufferCountHint: Int)
+    {
+        Reset("DilateErode.Initialize")
+        (BufferPool, _, OutputFormatDescription) = CreateBufferPool(From: FormatDescription, BufferCountHint: BufferCountHint)
+        if BufferPool == nil
+        {
+            print("BufferPool nil in DilateErode.Initialize.")
+            return
+        }
+        InputFormatDescription = FormatDescription
+        
+        Initialized = true
+        
+        var MetalTextureCache: CVMetalTextureCache? = nil
+        if CVMetalTextureCacheCreate(kCFAllocatorDefault, nil, MetalDevice!, nil, &MetalTextureCache) != kCVReturnSuccess
+        {
+            fatalError("Unable to allocation texture cache in DilateErode.")
+        }
+        else
+        {
+            TextureCache = MetalTextureCache
+        }
+    }
+    
+    func Reset(_ CalledBy: String = "")
+    {
+        objc_sync_enter(AccessLock)
+        defer{objc_sync_exit(AccessLock)}
+        BufferPool = nil
+        OutputFormatDescription = nil
+        InputFormatDescription = nil
+        TextureCache = nil
+        Initialized = false
+    }
+    
+    func Reset()
+    {
+        Reset("")
+    }
+    
+    var AccessLock = NSObject()
+    
+    var ParameterBuffer: MTLBuffer! = nil
+    var PreviousDebug = ""
+    
+    func Render(PixelBuffer: CVPixelBuffer) -> CVPixelBuffer?
+    {
+        if !Initialized
+        {
+            fatalError("DilateErode not initialized at Render(CVPixelBuffer) call.")
+        }
+        
+        //BufferPool is nil - nothing to do (or can do). This probably occurred because the user changed
+        //filters out from under us and the video sub-system hadn't quite caught up to the new filter and
+        //sent a frame to the no-longer-active filter.
+        if BufferPool == nil
+        {
+            return nil
+        }
+        
+        let WindowSize = ParameterManager.GetUInt1(From: ID(), Field: .WindowSize, Default: 5)
+        let ValDet = ParameterManager.GetUInt1(From: ID(), Field: .ValueDetermination, Default: 0)
+        let Op = ParameterManager.GetUInt1(From: ID(), Field: .Operation, Default: 0)
+        let Parameter = DEParameters(WindowSize: WindowSize, ValueDetermination: ValDet, Operation: Op)
+        let Parameters = [Parameter]
+        ParameterBuffer = MetalDevice!.makeBuffer(length: MemoryLayout<DEParameters>.size, options: [])
+        memcpy(ParameterBuffer.contents(), Parameters, MemoryLayout<DEParameters>.size)
+        
+        let ResultCount = 10
+        let ResultsBuffer = MetalDevice!.makeBuffer(length: MemoryLayout<ReturnBufferType>.stride * ResultCount, options: [])
+        let Results = UnsafeBufferPointer<ReturnBufferType>(start: UnsafePointer(ResultsBuffer!.contents().assumingMemoryBound(to: ReturnBufferType.self)),
+                                                            count: ResultCount)
+        
+        var NewPixelBuffer: CVPixelBuffer? = nil
+        CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, BufferPool!, &NewPixelBuffer)
+        guard let OutputBuffer = NewPixelBuffer else
+        {
+            print("Allocation failure for new pixel buffer pool in DilateErode.")
+            return nil
+        }
+        
+        guard let InputTexture = MakeTextureFromCVPixelBuffer(pixelBuffer: PixelBuffer, textureFormat: .bgra8Unorm),
+            let OutputTexture = MakeTextureFromCVPixelBuffer(pixelBuffer: OutputBuffer, textureFormat: .bgra8Unorm) else
+        {
+            print("Error creating textures in DilateErode.")
+            return nil
+        }
+        
+        guard let CommandQ = CommandQueue,
+            let CommandBuffer = CommandQ.makeCommandBuffer(),
+            let CommandEncoder = CommandBuffer.makeComputeCommandEncoder() else
+        {
+            print("Error creating Metal command queue.")
+            CVMetalTextureCacheFlush(TextureCache!, 0)
+            return nil
+        }
+        
+        CommandEncoder.label = "Dilate Erode Kernel"
+        CommandEncoder.setComputePipelineState(ComputePipelineState!)
+        CommandEncoder.setTexture(InputTexture, index: 0)
+        CommandEncoder.setTexture(OutputTexture, index: 1)
+        CommandEncoder.setBuffer(ParameterBuffer, offset: 0, index: 0)
+        
+                CommandEncoder.setBuffer(ResultsBuffer, offset: 0, index: 1)
+        
+        let w = ComputePipelineState!.threadExecutionWidth
+        let h = ComputePipelineState!.maxTotalThreadsPerThreadgroup / w
+        let ThreadsPerThreadGroup = MTLSize(width: w, height: h, depth: 1)
+        let ThreadGroupsPerGrid = MTLSize(width: (InputTexture.width + w - 1) / w,
+                                          height: (InputTexture.height + h - 1) / h,
+                                          depth: 1)
+        CommandEncoder.dispatchThreadgroups(ThreadGroupsPerGrid, threadsPerThreadgroup: ThreadsPerThreadGroup)
+        CommandEncoder.endEncoding()
+        CommandBuffer.commit()
+        
+        CommandBuffer.waitUntilCompleted()
+        
+        #if false
+        for V in Results
+        {
+            if V > 0
+            {
+                print("V=\(V)")
+            }
+        }
+        #endif
+        
+        return OutputBuffer
+    }
+    
+    var ImageDevice: MTLDevice? = nil
+    var InitializedForImage = false
+    private var ImageComputePipelineState: MTLComputePipelineState? = nil
+    private lazy var ImageCommandQueue: MTLCommandQueue? =
+    {
+        return self.MetalDevice?.makeCommandQueue()
+    }()
+    
+    func InitializeForImage()
+    {
+        ImageDevice = MTLCreateSystemDefaultDevice()
+        let DefaultLibrary = ImageDevice?.makeDefaultLibrary()
+        let KernelFunction = DefaultLibrary?.makeFunction(name: "DilateErodeKernel")
+        do
+        {
+            ImageComputePipelineState = try MetalDevice?.makeComputePipelineState(function: KernelFunction!)
+        }
+        catch
+        {
+            print("Unable to create pipeline state: \(error.localizedDescription)")
+        }
+        
+        InitializedForImage = true
+    }
+    
+    //http://flexmonkey.blogspot.com/2014/10/metal-kernel-functions-compute-shaders.html
+    func Render(Image: UIImage) -> UIImage?
+    {
+        if !InitializedForImage
+        {
+            fatalError("Not initialized.")
+        }
+        let CgImage = Image.cgImage
+        let ImageWidth: Int = (CgImage?.width)!
+        let ImageHeight: Int = (CgImage?.height)!
+        var RawData = [UInt8](repeating: 0, count: Int(ImageWidth * ImageHeight * 4))
+        let RGBColorSpace = CGColorSpaceCreateDeviceRGB()
+        let BitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue)
+        let Context = CGContext(data: &RawData, width: ImageWidth, height: ImageHeight, bitsPerComponent: (CgImage?.bitsPerComponent)!,
+                                bytesPerRow: (CgImage?.bytesPerRow)!, space: RGBColorSpace, bitmapInfo: BitmapInfo.rawValue)
+        Context!.draw(CgImage!, in: CGRect(x: 0, y: 0, width: CGFloat(ImageWidth), height: CGFloat(ImageHeight)))
+        let TextureDescriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: .rgba8Unorm,
+                                                                         width: Int(ImageWidth), height: Int(ImageHeight), mipmapped: true)
+        guard let Texture = ImageDevice?.makeTexture(descriptor: TextureDescriptor) else
+        {
+            print("Error creating input texture in DilateErode.Render.")
+            return nil
+        }
+        
+        let ResultCount = 10
+        let ResultsBuffer = MetalDevice!.makeBuffer(length: MemoryLayout<ReturnBufferType>.stride * ResultCount, options: [])
+        let Results = UnsafeBufferPointer<ReturnBufferType>(start: UnsafePointer(ResultsBuffer!.contents().assumingMemoryBound(to: ReturnBufferType.self)),
+                                                            count: ResultCount)
+        
+        let Region = MTLRegionMake2D(0, 0, Int(ImageWidth), Int(ImageHeight))
+        Texture.replace(region: Region, mipmapLevel: 0, withBytes: &RawData, bytesPerRow: Int((CgImage?.bytesPerRow)!))
+        let OutputTextureDescriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: Texture.pixelFormat,
+                                                                               width: Texture.width, height: Texture.height, mipmapped: true)
+        let OutputTexture = ImageDevice?.makeTexture(descriptor: OutputTextureDescriptor)
+        
+        let CommandBuffer = ImageCommandQueue?.makeCommandBuffer()
+        let CommandEncoder = CommandBuffer?.makeComputeCommandEncoder()
+        
+        CommandEncoder?.setComputePipelineState(ImageComputePipelineState!)
+        CommandEncoder?.setTexture(Texture, index: 0)
+        CommandEncoder?.setTexture(OutputTexture, index: 1)
+        
+                CommandEncoder?.setBuffer(ResultsBuffer, offset: 0, index: 1)
+        
+        let WindowSize = ParameterManager.GetUInt1(From: ID(), Field: .WindowSize, Default: 5)
+        let ValDet = ParameterManager.GetUInt1(From: ID(), Field: .ValueDetermination, Default: 0)
+        let Op = ParameterManager.GetUInt1(From: ID(), Field: .Operation, Default: 0)
+        let Parameter = DEParameters(WindowSize: WindowSize, ValueDetermination: ValDet, Operation: Op)
+        let Parameters = [Parameter]
+        ParameterBuffer = MetalDevice!.makeBuffer(length: MemoryLayout<DEParameters>.size, options: [])
+        memcpy(ParameterBuffer.contents(), Parameters, MemoryLayout<DEParameters>.size)
+        CommandEncoder!.setBuffer(ParameterBuffer, offset: 0, index: 0)
+        
+        let ThreadGroupCount  = MTLSizeMake(8, 8, 1)
+        let ThreadGroups = MTLSizeMake(Texture.width / ThreadGroupCount.width,
+                                       Texture.height / ThreadGroupCount.height,
+                                       1)
+        
+        ImageCommandQueue = ImageDevice?.makeCommandQueue()
+        
+        CommandEncoder!.dispatchThreadgroups(ThreadGroups, threadsPerThreadgroup: ThreadGroupCount)
+        CommandEncoder!.endEncoding()
+        CommandBuffer?.commit()
+        CommandBuffer?.waitUntilCompleted()
+        
+        #if false
+        for V in Results
+        {
+            if V > 0
+            {
+                print("V=\(V)")
+            }
+        }
+        #endif
+        
+        let ImageSize = CGSize(width: Texture.width, height: Texture.height)
+        let ImageByteCount = Int(ImageSize.width * ImageSize.height * 4)
+        let BytesPerRow = CgImage?.bytesPerRow
+        var ImageBytes = [UInt8](repeating: 0, count: ImageByteCount)
+        let ORegion = MTLRegionMake2D(0, 0, Int(ImageSize.width), Int(ImageSize.height))
+        OutputTexture?.getBytes(&ImageBytes, bytesPerRow: BytesPerRow!, from: ORegion, mipmapLevel: 0)
+        
+        let SizeOfUInt8 = UInt8.SizeOf()
+        let Provider = CGDataProvider(data: NSData(bytes: &ImageBytes, length: ImageBytes.count * SizeOfUInt8))
+        let OBitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue)
+        let RenderingIntent = CGColorRenderingIntent.defaultIntent
+        let FinalImage = CGImage(width: Int(ImageSize.width), height: Int(ImageSize.height),
+                                 bitsPerComponent: (CgImage?.bitsPerComponent)!, bitsPerPixel: (CgImage?.bitsPerPixel)!,
+                                 bytesPerRow: BytesPerRow!, space: RGBColorSpace, bitmapInfo: OBitmapInfo, provider: Provider!,
+                                 decode: nil, shouldInterpolate: false, intent: RenderingIntent)
+        LastUIImage = UIImage(cgImage: FinalImage!)
+        return UIImage(cgImage: FinalImage!)
+    }
+    
+    func Render(Image: CIImage) -> CIImage?
+    {
+        let UImage = UIImage(ciImage: Image)
+        if let IFinal = Render(Image: UImage)
+        {
+            if let CFinal = IFinal.ciImage
+            {
+                LastCIImage = CFinal
+                return CFinal
+            }
+            else
+            {
+                print("Error converting UIImage to CIImage in DilateErode.Render(CIImage)")
+                return nil
+            }
+        }
+        else
+        {
+            print("Error returned from Render(UIImage) in DilateErode.Render(CIImage)")
+            return nil
+        }
+    }
+    
+    var LastUIImage: UIImage? = nil
+    var LastCIImage: CIImage? = nil
+    
+    func LastImageRendered(AsUIImage: Bool) -> Any?
+    {
+        if AsUIImage
+        {
+            return LastUIImage as Any?
+        }
+        else
+        {
+            return LastCIImage as Any?
+        }
+    }
+    
+    func DefaultFieldValue(Field: FilterManager.InputFields) -> (FilterManager.InputTypes, Any?)
+    {
+        switch Field
+        {
+        case .WindowSize:
+            return (.IntType, 5 as Any?)
+            
+        case .ValueDetermination:
+            return (.IntType, 0 as Any?)
+            
+        case .Operation:
+            return (.IntType, 0 as Any?)
+            
+        default:
+            return (.NoType, nil)
+        }
+    }
+    
+    func SupportedFields() -> [FilterManager.InputFields]
+    {
+        return DilateErode.SupportedFields()
+    }
+    
+    public static func SupportedFields() -> [FilterManager.InputFields]
+    {
+        var Fields = [FilterManager.InputFields]()
+        Fields.append(.WindowSize)
+        Fields.append(.ValueDetermination)
+        Fields.append(.Operation)
+        return Fields
+    }
+    
+    func SettingsStoryboard() -> String?
+    {
+        return DilateErode.SettingsStoryboard()
+    }
+    
+    public static func SettingsStoryboard() -> String?
+    {
+        return "DilateErodeSettingsUI"
+    }
+    
+    func IsSlow() -> Bool
+    {
+        return false
+    }
+    
+    func FilterTarget() -> [FilterTargets]
+    {
+        return [.LiveView, .Video, .Still]
+    }
+}
