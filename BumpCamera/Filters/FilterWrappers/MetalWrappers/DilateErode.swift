161f0ac045e69a4e822287dda63c30329731438d
Stuart Rankin <Stuart@Norio4.local>
2019-02-17 22:59:19 +0900

    Update DilateErode.swift
    Added code to convert monochrome source images into RGB (but still grayscale) before applying still image filters.

--- BumpCamera/Filters/FilterWrappers/MetalWrappers/DilateErode.swift
+++ BumpCamera/Filters/FilterWrappers/MetalWrappers/DilateErode.swift
@@ -228,7 +228,23 @@ class DilateErode: FilterParent, Renderer
         {
             fatalError("Not initialized.")
         }
-        let CgImage = Image.cgImage
+        var CgImage = Image.cgImage
+        let ImageColorspace = CgImage?.colorSpace
+        //Handle sneaky grayscale images.
+        if ImageColorspace?.model == CGColorSpaceModel.monochrome
+        {
+            let NewColorSpace = CGColorSpaceCreateDeviceRGB()
+            let NewBMInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.noneSkipLast.rawValue)
+            let IWidth: Int = Int((CgImage?.width)!)
+            let IHeight: Int = Int((CgImage?.height)!)
+            var RawData = [UInt8](repeating: 0, count: Int(IWidth * IHeight * 4))
+            let GContext = CGContext(data: &RawData, width: IWidth, height: IHeight,
+                                     bitsPerComponent: 8, bytesPerRow: 4 * IWidth,
+                                     space: NewColorSpace, bitmapInfo: NewBMInfo.rawValue)
+            let ImageRect = CGRect(x: 0, y: 0, width: IWidth, height: IHeight)
+            GContext!.draw(CgImage!, in: ImageRect)
+            CgImage = GContext!.makeImage()
+        }
         let ImageWidth: Int = (CgImage?.width)!
         let ImageHeight: Int = (CgImage?.height)!
         var RawData = [UInt8](repeating: 0, count: Int(ImageWidth * ImageHeight * 4))
