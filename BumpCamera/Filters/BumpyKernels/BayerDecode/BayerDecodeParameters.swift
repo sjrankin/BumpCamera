1d1075e97d3d40b50362783771038517f0d9eaf9
Stuart Rankin <Stuart@Norio4.local>
2019-02-17 22:58:05 +0900

    Create BayerDecodeParameters.swift
    Parameters to control the Bayer Decode Metal kernel.

---
+++ BumpCamera/Filters/BumpyKernels/BayerDecode/BayerDecodeParameters.swift
@@ -0,0 +1,16 @@
+//
+//  BayerDecodeParameters.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/17/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import simd
+
+struct BayerDecodeParameters
+{
+    let Order: simd_uint1
+    let Method: simd_uint1
+}
