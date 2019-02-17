0c837b5128aa3444618c0da7e0f0a8aee277e5f4
Stuart Rankin <Stuart@Norio4.local>
2019-02-17 23:02:16 +0900

    Create SepiaTone.swift
    Swift wrapper around the Sepia Tone filter.

---
+++ BumpCamera/Filters/FilterWrappers/SepiaTone.swift
@@ -0,0 +1,241 @@
+//
+//  SepiaTone.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/16/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreMedia
+import CoreVideo
+import CoreImage
+
+class SepiaTone: FilterParent, Renderer
+{
+    static let _ID: UUID = UUID(uuidString: "28c09c6a-3ca1-465c-a872-11b4033a9aca")!
+    
+    func ID() -> UUID
+    {
+        return SepiaTone._ID
+    }
+    
+    static func ID() -> UUID
+    {
+        return _ID
+    }
+    
+    var InstanceID: UUID
+    {
+        return UUID()
+    }
+    
+    var Description: String = "SepiaTone"
+    
+    var IconName: String = "SepiaTone"
+    
+    var Initialized = false
+    
+    private var PrimaryFilter: CIFilter? = nil
+    
+    private var SecondaryFilter: CIFilter? = nil
+    
+    private var InvertFilter: CIFilter? = nil
+    
+    private var AlphaMaskFilter: CIFilter? = nil
+    
+    private var MergeSourceAtop: CIFilter? = nil
+    
+    private var Context: CIContext? = nil
+    
+    private var BufferPool: CVPixelBufferPool? = nil
+    
+    private var ColorSpace: CGColorSpace? = nil
+    
+    private(set) var OutputFormatDescription: CMFormatDescription? = nil
+    
+    private(set) var InputFormatDescription: CMFormatDescription? = nil
+    
+    func Initialize(With FormatDescription: CMFormatDescription, BufferCountHint: Int)
+    {
+        Reset("SepiaTone.Initialize")
+        (BufferPool, ColorSpace, OutputFormatDescription) = CreateBufferPool(From: FormatDescription, BufferCountHint: BufferCountHint)
+        if BufferPool == nil
+        {
+            return
+        }
+        InputFormatDescription = FormatDescription
+        Context = CIContext()
+        PrimaryFilter = CIFilter(name: "CISepiaTone")
+        Initialized = true
+    }
+    
+    func Reset(_ CalledBy: String = "")
+    {
+        objc_sync_enter(AccessLock)
+        defer{objc_sync_exit(AccessLock)}
+        Context = nil
+        PrimaryFilter = nil
+        SecondaryFilter = nil
+        InvertFilter = nil
+        AlphaMaskFilter = nil
+        MergeSourceAtop = nil
+        ColorSpace = nil
+        BufferPool = nil
+        OutputFormatDescription = nil
+        InputFormatDescription = nil
+        Initialized = false
+    }
+    
+    func Reset()
+    {
+        Reset("")
+    }
+    
+    var AccessLock = NSObject()
+    
+    func Render(PixelBuffer: CVPixelBuffer) -> CVPixelBuffer?
+    {
+        objc_sync_enter(AccessLock)
+        defer{objc_sync_exit(AccessLock)}
+        guard let PrimaryFilter = PrimaryFilter,
+            let Context = Context,
+            Initialized else
+        {
+            print("Filter not initialized.")
+            return nil
+        }
+        
+        let SourceImage = CIImage(cvImageBuffer: PixelBuffer)
+        PrimaryFilter.setDefaults()
+        PrimaryFilter.setValue(SourceImage, forKey: kCIInputImageKey)
+        
+        let Levels = ParameterManager.GetDouble(From: ID(), Field: .SepiaToneLevel, Default: 1.0)
+        PrimaryFilter.setValue(Levels, forKey: "inputIntensity")
+        
+        guard let FilteredImage = PrimaryFilter.value(forKey: kCIOutputImageKey) as? CIImage else
+        {
+            print("CIFilter failed to render image.")
+            return nil
+        }
+        
+        var PixBuf: CVPixelBuffer?
+        CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, BufferPool!, &PixBuf)
+        guard let OutPixBuf = PixBuf else
+        {
+            print("Allocation failure in SepiaTone.")
+            return nil
+        }
+        
+        Context.render(FilteredImage, to: OutPixBuf, bounds: FilteredImage.extent, colorSpace: ColorSpace)
+        return OutPixBuf
+    }
+    
+    func InitializeForImage()
+    {
+    }
+    
+    func Render(Image: UIImage) -> UIImage?
+    {
+        if let CImage = CIImage(image: Image)
+        {
+            if let Result = Render(Image: CImage)
+            {
+                LastCIImage = Result
+                let Final = UIImage(ciImage: Result)
+                LastUIImage = Final
+                return Final
+            }
+            else
+            {
+                print("Error returned Render.")
+                return nil
+            }
+        }
+        else
+        {
+            print("Error converting UIImage to CIImage.")
+            return nil
+        }
+    }
+    
+    func Render(Image: CIImage) -> CIImage?
+    {
+        objc_sync_enter(AccessLock)
+        defer{objc_sync_exit(AccessLock)}
+        
+        PrimaryFilter = CIFilter(name: "CISepiaTone")
+        PrimaryFilter?.setDefaults()
+        PrimaryFilter?.setValue(Image, forKey: kCIInputImageKey)
+        
+        let Levels = ParameterManager.GetDouble(From: ID(), Field: .SepiaToneLevel, Default: 1.0)
+        PrimaryFilter?.setValue(Levels, forKey: "inputIntensity")
+        
+        if let Result = PrimaryFilter?.value(forKey: kCIOutputImageKey) as? CIImage
+        {
+            LastCIImage = Result
+            return Result
+        }
+        return nil
+    }
+    
+    var LastUIImage: UIImage? = nil
+    var LastCIImage: CIImage? = nil
+    
+    func LastImageRendered(AsUIImage: Bool) -> Any?
+    {
+        if AsUIImage
+        {
+            return LastUIImage as Any?
+        }
+        else
+        {
+            return LastCIImage as Any?
+        }
+    }
+    
+    func DefaultFieldValue(Field: FilterManager.InputFields) -> (FilterManager.InputTypes, Any?)
+    {
+        switch Field
+        {
+        case .SepiaToneLevel:
+            return (.DoubleType, 1.0 as Any?)
+            
+        default:
+            fatalError("Unexpected field \(Field) encountered in DefaultFieldValue.")
+        }
+    }
+    
+    func SupportedFields() -> [FilterManager.InputFields]
+    {
+        return SepiaTone.SupportedFields()
+    }
+    
+    public static func SupportedFields() -> [FilterManager.InputFields]
+    {
+        var Fields = [FilterManager.InputFields]()
+        Fields.append(.SepiaToneLevel)
+        return Fields
+    }
+    
+    func SettingsStoryboard() -> String?
+    {
+        return SepiaTone.SettingsStoryboard()
+    }
+    
+    public static func SettingsStoryboard() -> String?
+    {
+        return "SepiaToneSettingsUI"
+    }
+    
+    func IsSlow() -> Bool
+    {
+        return false
+    }
+    
+    func FilterTarget() -> [FilterTargets]
+    {
+        return [.LiveView, .Video, .Still]
+    }
+}
