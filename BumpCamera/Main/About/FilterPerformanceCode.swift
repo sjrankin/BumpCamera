b4056ec9576c35a68f912f38d18fbdf13897ce1d
Stuart Rankin <Stuart@Norio4.local>
2019-02-18 22:59:52 +0900

    Create FilterPerformanceCode.swift
    Start of code to run the filter performance view controller.

---
+++ BumpCamera/Main/About/FilterPerformanceCode.swift
@@ -0,0 +1,18 @@
+//
+//  FilterPerformanceCode.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/18/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FilterPerformanceCode: UITableViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
