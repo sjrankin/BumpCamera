84efd5b199015aed9ca890b27d11d4cd7a0757b4
Stuart Rankin <Stuart@Norio4.local>
2019-02-19 21:15:21 +0900

    Create AboutCode.swift
    Code behind the About view controller. Exists at this point solely to add an empty table footer view to clear up the UI clutter.

---
+++ BumpCamera/Main/About/AboutCode.swift
@@ -0,0 +1,19 @@
+//
+//  AboutCode.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/19/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AboutCode: UITableViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+    }
+}
