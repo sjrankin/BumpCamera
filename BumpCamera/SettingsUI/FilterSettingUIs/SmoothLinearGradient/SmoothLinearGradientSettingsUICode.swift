ab5b0c67307dcd76374f5f7f8edb29394b817d9a
Stuart Rankin <Stuart@Norio4.local>
2019-02-20 23:05:14 +0900

    Update SmoothLinearGradientSettingsUICode.swift
    Updated generate image size UI from sliders to segment controls.
    
    Minor automatic formatting changes to spacing.
    
    Cosmetic changes to gradient point reporting.
    
    Added code to respond to the new Save button in the UI - the code saves the generated image in the photo roll.

--- BumpCamera/SettingsUI/FilterSettingUIs/SmoothLinearGradient/SmoothLinearGradientSettingsUICode.swift
+++ BumpCamera/SettingsUI/FilterSettingUIs/SmoothLinearGradient/SmoothLinearGradientSettingsUICode.swift
@@ -11,7 +11,7 @@ import UIKit
 
 class SmoothLinearGradientSettingsUICode: FilterSettingUIBase, ColorPickerProtocol
 {
-
+    
     
     override func viewDidLoad()
     {
@@ -44,19 +44,26 @@ class SmoothLinearGradientSettingsUICode: FilterSettingUIBase, ColorPickerProtoc
         let V0 = ParameterManager.GetVector(From: FilterID, Field: .Point0, Default: CIVector(x: 0, y: 0))
         Color1XSlider.value = Float(V0.x * 1000.0)
         Color1YSlider.value = Float(V0.y * 1000.0)
-        Color1XValue.text = "\(V0.x.Round(To: 3))"
-        Color1YValue.text = "\(V0.y.Round(To: 3))"
+        Color1XValue.text = "\((V0.x * 100.0).Round(To: 2))%"
+        Color1YValue.text = "\((V0.y * 100.0).Round(To: 2))%"
         let V1 = ParameterManager.GetVector(From: FilterID, Field: .Point1, Default: CIVector(x: 0, y: 0))
         Color2XSlider.value = Float(V1.x * 1000.0)
         Color2YSlider.value = Float(V1.y * 1000.0)
-        Color2XValue.text = "\(V1.x.Round(To: 3))"
-        Color2YValue.text = "\(V1.y.Round(To: 3))"
+        Color2XValue.text = "\((V1.x * 100.0).Round(To: 2))%"
+        Color2YValue.text = "\((V1.y * 100.0).Round(To: 2))%"
         let ImageWidth = ParameterManager.GetInt(From: FilterID, Field: .IWidth, Default: 1024)
         let ImageHeight = ParameterManager.GetInt(From: FilterID, Field: .IHeight, Default: 1024)
         let WidthSegment = IndexOfClosestTo(ImageWidth, [256, 512, 1024, 2048, 4096])
+        //print("Closest segment to \(ImageWidth) is \(WidthSegment)")
         let HeightSegment = IndexOfClosestTo(ImageHeight, [256, 512, 1024, 2048, 4096])
+        //print("Closest segment to \(ImageHeight) is \(HeightSegment)")
         WidthSegments.selectedSegmentIndex = WidthSegment
         HeightSegments.selectedSegmentIndex = HeightSegment
+        
+        //Because the base class doesn't have a standard image to display with a generator filter,
+        //we need to force it to display the output of the current generator immediately or the user
+        //will see only a black rectangle until he changes a setting.
+        ShowSampleView()
     }
     
     func IndexOfClosestTo(_ Find: Int, _ List: [Int]) -> Int
@@ -110,36 +117,36 @@ class SmoothLinearGradientSettingsUICode: FilterSettingUIBase, ColorPickerProtoc
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
-    switch segue.identifier
-    {
-    case "ToColor1ColorPicker":
-        let EditMe = ParameterManager.GetColor(From: FilterID, Field: .Color0, Default: UIColor.red)
-        if let Dest = segue.destination as? ColorPicker
+        switch segue.identifier
         {
-            Dest.delegate = self
-            Dest.ColorToEdit(EditMe, Tag: "Color1")
-        }
-        else
-        {
-            print("Error getting destination for color picker from segue.")
-            return
-        }
-        
-    case "ToColor2ColorPicker":
-        let EditMe = ParameterManager.GetColor(From: FilterID, Field: .Color1, Default: UIColor.red)
-        if let Dest = segue.destination as? ColorPicker
-        {
-            Dest.delegate = self
-            Dest.ColorToEdit(EditMe, Tag: "Color2")
-        }
-        else
-        {
-            print("Error getting destination for color picker from segue.")
-            return
-        }
-        
-    default:
-        break
+        case "ToColor1ColorPicker":
+            let EditMe = ParameterManager.GetColor(From: FilterID, Field: .Color0, Default: UIColor.red)
+            if let Dest = segue.destination as? ColorPicker
+            {
+                Dest.delegate = self
+                Dest.ColorToEdit(EditMe, Tag: "Color1")
+            }
+            else
+            {
+                print("Error getting destination for color picker from segue.")
+                return
+            }
+            
+        case "ToColor2ColorPicker":
+            let EditMe = ParameterManager.GetColor(From: FilterID, Field: .Color1, Default: UIColor.red)
+            if let Dest = segue.destination as? ColorPicker
+            {
+                Dest.delegate = self
+                Dest.ColorToEdit(EditMe, Tag: "Color2")
+            }
+            else
+            {
+                print("Error getting destination for color picker from segue.")
+                return
+            }
+            
+        default:
+            break
         }
         
         super.prepare(for: segue, sender: self)
@@ -148,7 +155,7 @@ class SmoothLinearGradientSettingsUICode: FilterSettingUIBase, ColorPickerProtoc
     @objc func HandleColor1XStoppedSliding()
     {
         let SliderValue = Double(Color1XSlider.value / 1000.0)
-        Color1XValue.text = "\(SliderValue.Round(To: 3))"
+        Color1XValue.text = "\((SliderValue * 100.0).Round(To: 2))%"
         let Y1SliderValue = CGFloat(Color1YSlider.value / 1000.0)
         UpdateValue(WithValue: CIVector(x: CGFloat(SliderValue), y: Y1SliderValue), ToField: .Point0)
         ShowSampleView()
@@ -157,13 +164,13 @@ class SmoothLinearGradientSettingsUICode: FilterSettingUIBase, ColorPickerProtoc
     @IBAction func HandleColor1XChanged(_ sender: Any)
     {
         let SliderValue = Double(Color1XSlider.value / 1000.0)
-        Color1XValue.text = "\(SliderValue.Round(To: 3))"
+        Color1XValue.text = "\((SliderValue * 100.0).Round(To: 2))%"
     }
     
     @objc func HandleColor1YStoppedSliding()
     {
         let SliderValue = Double(Color1YSlider.value / 1000.0)
-        Color1YValue.text = "\(SliderValue.Round(To: 3))"
+        Color1YValue.text = "\((SliderValue * 100.0).Round(To: 2))%"
         let X1SliderValue = CGFloat(Color1XSlider.value / 1000.0)
         UpdateValue(WithValue: CIVector(x: X1SliderValue, y: CGFloat(SliderValue)), ToField: .Point0)
         ShowSampleView()
@@ -172,13 +179,13 @@ class SmoothLinearGradientSettingsUICode: FilterSettingUIBase, ColorPickerProtoc
     @IBAction func HandleColor1YChanged(_ sender: Any)
     {
         let SliderValue = Double(Color1YSlider.value / 1000.0)
-        Color1YValue.text = "\(SliderValue.Round(To: 3))"
+        Color1YValue.text = "\((SliderValue * 100.0).Round(To: 2))%"
     }
     
     @objc func HandleColor2XStoppedSliding()
     {
         let SliderValue = Double(Color2XSlider.value / 1000.0)
-        Color2XValue.text = "\(SliderValue.Round(To: 3))"
+        Color2XValue.text = "\((SliderValue * 100.0).Round(To: 2))%"
         let Y2SliderValue = CGFloat(Color2YSlider.value / 1000.0)
         UpdateValue(WithValue: CIVector(x: CGFloat(SliderValue), y: Y2SliderValue), ToField: .Point0)
         ShowSampleView()
@@ -187,39 +194,65 @@ class SmoothLinearGradientSettingsUICode: FilterSettingUIBase, ColorPickerProtoc
     @IBAction func HandleColor2XChanged(_ sender: Any)
     {
         let SliderValue = Double(Color2XSlider.value / 1000.0)
-        Color2XValue.text = "\(SliderValue.Round(To: 3))"
-        
+        Color2XValue.text = "\((SliderValue * 100.0).Round(To: 2))%"
     }
     
     @objc func HandleColor2YStoppedSliding()
     {
-        let SliderValue = Double(Color2XSlider.value / 1000.0)
-        Color2XValue.text = "\(SliderValue.Round(To: 3))"
-        let Y2SliderValue = CGFloat(Color2YSlider.value / 1000.0)
-        UpdateValue(WithValue: CIVector(x: CGFloat(SliderValue), y: Y2SliderValue), ToField: .Point1)
+        let SliderValue = Double(Color2YSlider.value / 1000.0)
+        Color2YValue.text = "\((SliderValue * 100.0).Round(To: 2))%"
+        let X2SliderValue = CGFloat(Color2XSlider.value / 1000.0)
+        UpdateValue(WithValue: CIVector(x: X2SliderValue, y: CGFloat(SliderValue)), ToField: .Point1)
         ShowSampleView()
     }
     
     @IBAction func HandleColor2YChanged(_ sender: Any)
     {
-        let SliderValue = Double(Color2XSlider.value / 1000.0)
-        Color2XValue.text = "\(SliderValue.Round(To: 3))"
+        let SliderValue = Double(Color2YSlider.value / 1000.0)
+        Color2YValue.text = "\((SliderValue * 100.0).Round(To: 2))%"
     }
     
     @IBAction func HandleHeightChanged(_ sender: Any)
     {
         let HeightIndex = HeightSegments.selectedSegmentIndex
         let ImageHeight: Int = Int((pow(Double(2), Double(8 + HeightIndex))))
+        //print("New image height is \(ImageHeight)")
         UpdateValue(WithValue: ImageHeight, ToField: .IHeight)
         ShowSampleView()
     }
     
     @IBAction func HandleWidthChanged(_ sender: Any)
     {
-        let HeightIndex = HeightSegments.selectedSegmentIndex
-        let ImageHeight: Int = Int((pow(Double(2), Double(8 + HeightIndex))))
-        UpdateValue(WithValue: ImageHeight, ToField: .IHeight)
+        let WidthIndex = WidthSegments.selectedSegmentIndex
+        let ImageWidth: Int = Int((pow(Double(2), Double(8 + WidthIndex))))
+        UpdateValue(WithValue: ImageWidth, ToField: .IWidth)
+        //print("New image width is \(ImageWidth)")
         ShowSampleView()
+    }
+    
+    @IBAction func HandleSaveImageButton(_ sender: Any)
+    {
+        //Need to convert the generated image from a CIImage to a UIImage first...
+        let Context = CIContext()
+        let cgimg = Context.createCGImage(LastGeneratedImage, from: LastGeneratedImage.extent)
+        let SaveMe = UIImage(cgImage: cgimg!)
+        UIImageWriteToSavedPhotosAlbum(SaveMe, self, #selector(image), nil)
+    }
+    
+    @objc override func image(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer)
+    {
+        if let SaveError = error
+        {
+            let Alert = UIAlertController(title: "Image Save Error", message: SaveError.localizedDescription, preferredStyle: .alert)
+            Alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(Alert, animated: true)
+        }
+        else
+        {
+            let Alert = UIAlertController(title: "Generated Image Saved", message: "The generated image with current effect parameters has been saved to the photo album.", preferredStyle: .alert)
+            Alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(Alert, animated: true)
+        }
     }
     
     @IBOutlet weak var HeightSegments: UISegmentedControl!
