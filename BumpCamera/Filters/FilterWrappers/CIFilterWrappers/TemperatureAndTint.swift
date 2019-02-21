aa7d205470d478057fea25348859c4cad21a0e5a
Stuart Rankin <Stuart@Norio4.local>
2019-02-21 22:38:43 +0900

    Create TemperatureAndTint.swift
    Swift wrapper around the CITemperatureAndTint CIFilter.

---
+++ BumpCamera/Filters/FilterWrappers/CIFilterWrappers/TemperatureAndTint.swift
@@ -0,0 +1,395 @@
+//
+//  TemperatureAndTint.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/21/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreMedia
+import CoreVideo
+import CoreImage
+
+//https://stackoverflow.com/questions/11464266/citemperatureandtint-for-image-in-ios
+class TemperatureAndTint: FilterParent, Renderer
+{
+    static let _ID: UUID = UUID(uuidString: "ae7c6800-45d0-454b-a339-67343685b9b2")!
+    
+    func ID() -> UUID
+    {
+        return TemperatureAndTint._ID
+    }
+    
+    static func ID() -> UUID
+    {
+        return _ID
+    }
+    
+    static func Title() -> String
+    {
+        return "Temp & Tint"
+    }
+    
+    func Title() -> String
+    {
+        return TemperatureAndTint.Title()
+    }
+    
+    var InstanceID: UUID
+    {
+        return UUID()
+    }
+    
+    var Description: String = "Temp & Tint"
+    
+    var IconName: String = "Temp & Tint"
+    
+    var Initialized = false
+    
+    private var PrimaryFilter: CIFilter? = nil
+    
+    private var SecondaryFilter: CIFilter? = nil
+    
+    private var InvertFilter: CIFilter? = nil
+    
+    private var AlphaMaskFilter: CIFilter? = nil
+    
+    private var MergeSourceAtop: CIFilter? = nil
+    
+    private var Context: CIContext? = nil
+    
+    private var BufferPool: CVPixelBufferPool? = nil
+    
+    private var ColorSpace: CGColorSpace? = nil
+    
+    private(set) var OutputFormatDescription: CMFormatDescription? = nil
+    
+    private(set) var InputFormatDescription: CMFormatDescription? = nil
+    
+    func Initialize(With FormatDescription: CMFormatDescription, BufferCountHint: Int)
+    {
+        Reset("TemperatureAndTint.Initialize")
+        (BufferPool, ColorSpace, OutputFormatDescription) = CreateBufferPool(From: FormatDescription, BufferCountHint: BufferCountHint)
+        if BufferPool == nil
+        {
+            return
+        }
+        InputFormatDescription = FormatDescription
+        Context = CIContext()
+        PrimaryFilter = CIFilter(name: "CITemperatureAndTint")
+        Initialized = true
+    }
+    
+    func Reset(_ CalledBy: String = "")
+    {
+        objc_sync_enter(AccessLock)
+        defer{objc_sync_exit(AccessLock)}
+        Context = nil
+        PrimaryFilter = nil
+        SecondaryFilter = nil
+        InvertFilter = nil
+        AlphaMaskFilter = nil
+        MergeSourceAtop = nil
+        ColorSpace = nil
+        BufferPool = nil
+        OutputFormatDescription = nil
+        InputFormatDescription = nil
+        Initialized = false
+    }
+    
+    func Reset()
+    {
+        Reset("")
+    }
+    
+    var AccessLock = NSObject()
+    
+    func Render(PixelBuffer: CVPixelBuffer) -> CVPixelBuffer?
+    {
+        objc_sync_enter(AccessLock)
+        defer{objc_sync_exit(AccessLock)}
+        guard let PrimaryFilter = PrimaryFilter,
+            let Context = Context,
+            Initialized else
+        {
+            print("Filter not initialized.")
+            return nil
+        }
+        
+        let Start = CACurrentMediaTime()
+        let SourceImage = CIImage(cvImageBuffer: PixelBuffer)
+        PrimaryFilter.setDefaults()
+        PrimaryFilter.setValue(SourceImage, forKey: kCIInputImageKey)
+        
+        let SourcePoint = ParameterManager.GetVector(From: ID(), Field: .SourcePoint, Default: CIVector(x: 0, y: 0))
+        let TargetPoint = ParameterManager.GetVector(From: ID(), Field: .TargetPoint, Default: CIVector(x: 0, y: 0))
+        let LocateSource = ParameterManager.GetBool(From: ID(), Field: .AutoLocateSource, Default: true)
+        let LocateTarget = ParameterManager.GetBool(From: ID(), Field: .AutoLocateTarget, Default: true)
+        
+        PrimaryFilter.setValue(SourcePoint, forKey: "inputNeutral")
+        PrimaryFilter.setValue(TargetPoint, forKey: "InputTargetNeutral")
+        
+        guard let FilteredImage = PrimaryFilter.value(forKey: kCIOutputImageKey) as? CIImage else
+        {
+            print("CIFilter failed to render image.")
+            return nil
+        }
+        
+        var PixBuf: CVPixelBuffer?
+        CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, BufferPool!, &PixBuf)
+        guard let OutPixBuf = PixBuf else
+        {
+            print("Allocation failure in TemperatureAndTint.")
+            return nil
+        }
+        
+        Context.render(FilteredImage, to: OutPixBuf, bounds: FilteredImage.extent, colorSpace: ColorSpace)
+        
+        LiveRenderTime = CACurrentMediaTime() - Start
+        ParameterManager.UpdateRenderAccumulator(NewValue: LiveRenderTime, ID: ID(), ForImage: false)
+        return OutPixBuf
+    }
+    
+    func InitializeForImage()
+    {
+    }
+    
+    func Render(Image: UIImage) -> UIImage?
+    {
+        if let CImage = CIImage(image: Image)
+        {
+            if let Result = Render(Image: CImage)
+            {
+                LastCIImage = Result
+                let Final = UIImage(ciImage: Result)
+                LastUIImage = Final
+                return Final
+            }
+            else
+            {
+                print("Error returned Render.")
+                return nil
+            }
+        }
+        else
+        {
+            print("Error converting UIImage to CIImage.")
+            return nil
+        }
+    }
+    
+    func Render(Image: CIImage) -> CIImage?
+    {
+        objc_sync_enter(AccessLock)
+        defer{objc_sync_exit(AccessLock)}
+        
+        let Start = CACurrentMediaTime()
+        PrimaryFilter = CIFilter(name: "CITemperatureAndTint")
+        PrimaryFilter?.setDefaults()
+        PrimaryFilter?.setValue(Image, forKey: kCIInputImageKey)
+        
+
+        let SourcePoint = ParameterManager.GetVector(From: ID(), Field: .SourcePoint, Default: CIVector(x: 0, y: 0))
+        let TargetPoint = ParameterManager.GetVector(From: ID(), Field: .TargetPoint, Default: CIVector(x: 0, y: 0))
+        let LocateSource = ParameterManager.GetBool(From: ID(), Field: .AutoLocateSource, Default: true)
+        let LocateTarget = ParameterManager.GetBool(From: ID(), Field: .AutoLocateTarget, Default: true)
+        
+        PrimaryFilter?.setValue(SourcePoint, forKey: "inputNeutral")
+        PrimaryFilter?.setValue(TargetPoint, forKey: "InputTargetNeutral")
+        
+        if let Result = PrimaryFilter?.value(forKey: kCIOutputImageKey) as? CIImage
+        {
+            LastCIImage = Result
+            ImageRenderTime = CACurrentMediaTime() - Start
+            ParameterManager.UpdateRenderAccumulator(NewValue: ImageRenderTime, ID: ID(), ForImage: true)
+            return Result
+        }
+        return nil
+    }
+    
+    /// Returns the generated image. If the filter does not support generated images nil is returned.
+    ///
+    /// - Returns: Nil is always returned.
+    func Generate() -> CIImage?
+    {
+        return nil
+    }
+    
+    var LastUIImage: UIImage? = nil
+    var LastCIImage: CIImage? = nil
+    
+    func LastImageRendered(AsUIImage: Bool) -> Any?
+    {
+        if AsUIImage
+        {
+            return LastUIImage as Any?
+        }
+        else
+        {
+            return LastCIImage as Any?
+        }
+    }
+    
+    func DefaultFieldValue(Field: FilterManager.InputFields) -> (FilterManager.InputTypes, Any?)
+    {
+        switch Field
+        {
+        case .SourcePoint:
+            return (.PointType, CGPoint(x: 0, y: 0) as Any?)
+            
+        case .TargetPoint:
+            return (.PointType, CGPoint(x: 0, y: 0) as Any?)
+            
+        case .AutoLocateTarget:
+            return (.BoolType, true as Any?)
+            
+        case .AutoLocateSource:
+            return (.BoolType, true as Any?)
+            
+        case .RenderImageCount:
+            return (.IntType, 0 as Any?)
+            
+        case .CumulativeImageRenderDuration:
+            return (.DoubleType, 0.0 as Any?)
+            
+        case .RenderLiveCount:
+            return (.IntType, 0 as Any?)
+            
+        case .CumulativeLiveRenderDuration:
+            return (.DoubleType, 0.0 as Any?)
+            
+        default:
+            fatalError("Unexpected field \(Field) encountered in DefaultFieldValue.")
+        }
+    }
+    
+    func SupportedFields() -> [FilterManager.InputFields]
+    {
+        return TemperatureAndTint.SupportedFields()
+    }
+    
+    public static func SupportedFields() -> [FilterManager.InputFields]
+    {
+        var Fields = [FilterManager.InputFields]()
+        Fields.append(.SourcePoint)
+        Fields.append(.TargetPoint)
+        Fields.append(.AutoLocateSource)
+        Fields.append(.AutoLocateTarget)
+        Fields.append(.RenderImageCount)
+        Fields.append(.CumulativeImageRenderDuration)
+        Fields.append(.RenderLiveCount)
+        Fields.append(.CumulativeLiveRenderDuration)
+        return Fields
+    }
+    
+    func SettingsStoryboard() -> String?
+    {
+        return TemperatureAndTint.SettingsStoryboard()
+    }
+    
+    public static func SettingsStoryboard() -> String?
+    {
+        return "TemperatureAndTintSettingsUI"
+    }
+    
+    func IsSlow() -> Bool
+    {
+        return false
+    }
+    
+    public static func FilterTarget() -> [FilterTargets]
+    {
+        return [.LiveView, .Video, .Still]
+    }
+    
+    func FilterTarget() -> [FilterTargets]
+    {
+        return TemperatureAndTint.FilterTarget()
+    }
+    
+    private var ImageRenderTime: Double = 0.0
+    private var LiveRenderTime: Double = 0.0
+    
+    /// Return the rendering time for the most recent image or live view render. Optionally reset the saved render
+    /// time. Render time is from start of function call to return, not just kernel/filter time. One data point isn't
+    /// terribly useful so be sure to collect several hundred in different environmental conditions to get a good idea
+    /// of the actual rendering time.
+    ///
+    /// - Parameters:
+    ///   - ForImage: If true, return the render time for the last image rendered. If false, return the render time
+    ///               for the last live view frame rendered.
+    ///   - Reset: If true, the render time is reset to 0.0 for the type of data being returned.
+    /// - Returns: The number of seconds it took to render the for the type of data specified by ForImage.
+    public func RenderTime(ForImage: Bool, Reset: Bool = false) -> Double
+    {
+        let Final = ForImage ? ImageRenderTime : LiveRenderTime
+        if Reset
+        {
+            if ForImage
+            {
+                ParameterManager.SetField(To: ID(), Field: .RenderImageCount, Value: 0)
+                ParameterManager.SetField(To: ID(), Field: .CumulativeImageRenderDuration, Value: 0.0)
+            }
+            else
+            {
+                ParameterManager.SetField(To: ID(), Field: .RenderLiveCount, Value: 0)
+                ParameterManager.SetField(To: ID(), Field: .CumulativeLiveRenderDuration, Value: 0.0)
+            }
+        }
+        return Final
+    }
+    
+    /// Returns a list of strings intended to be used as key words in Exif data.
+    ///
+    /// - Returns: List of key words associated with the filter, including setting values.
+    func ExifKeyWords() -> [String]
+    {
+        var Keywords = [String]()
+        Keywords.append("Filter: CITemperatureAndTint")
+        Keywords.append("FilterType: CIFilter")
+        Keywords.append("FilterID: \(ID().uuidString)")
+        return Keywords
+    }
+    
+    /// Returns a dictionary of Exif key-value pairs. Intended for use for inclusion in image Exif data.
+    ///
+    /// - Returns: Dictionary of key-value data for top-level Exif data.
+    func ExifKeyValues() -> [String: String]
+    {
+        return [String: String]()
+    }
+    
+    var FilterKernel: FilterManager.FilterKernelTypes
+    {
+        get
+        {
+            return TemperatureAndTint.FilterKernel
+        }
+    }
+    
+    static var FilterKernel: FilterManager.FilterKernelTypes
+    {
+        get
+        {
+            return FilterManager.FilterKernelTypes.CIFilter
+        }
+    }
+    
+    /// Describes the available ports for the filter. Static version.
+    ///
+    /// - Returns: Array of ports.
+    static func Ports() -> [FilterPorts]
+    {
+        return [FilterPorts.Input, FilterPorts.Output]
+    }
+    
+    /// Describes the available ports for the filter.
+    ///
+    /// - Returns: Array of ports.
+    func Ports() -> [FilterPorts]
+    {
+        return TemperatureAndTint.Ports()
+    }
+}
