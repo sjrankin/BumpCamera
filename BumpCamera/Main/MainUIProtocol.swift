f86e50ea8f055edef77e1f857cc30e36abed4279
Stuart Rankin <Stuart@Norio4.local>
2019-02-23 20:03:22 +0900

    Create MainUIProtocol.swift
    Protocol to define communication between child objects and the MainUI. Initial version defines UserFavoritesChanged to tell the MainUI when the user changes a rating for fave on a filter so the MainUI can redraw the filter selection controls.

---
+++ BumpCamera/Main/MainUIProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  MainUIProtocol.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/23/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+protocol MainUIProtocol
+{
+    /// Notifies the main UI that user favorites were changed one way or another.
+    func UserFavoritesChanged()
+}
