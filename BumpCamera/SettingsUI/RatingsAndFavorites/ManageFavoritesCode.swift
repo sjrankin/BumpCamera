c3774cdefe9f8c92a02649303719a4954a7add24
Stuart Rankin <Stuart@Norio4.local>
2019-02-24 23:03:28 +0900

    Create ManageFavoritesCode.swift
    Code to run storyboard to manage favorites.

---
+++ BumpCamera/SettingsUI/RatingsAndFavorites/ManageFavoritesCode.swift
@@ -0,0 +1,25 @@
+//
+//  ManageFavoritesCode.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/24/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ManageFavoritesCode: UITableViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        self.tableView.tableFooterView = UIView()
+    }
+    
+    @IBAction func HandleBackButton(_ sender: Any)
+    {
+        dismiss(animated: true, completion: nil)
+    }
+}
