5f6561c25f91b180ea9fa66792ebff0f8340e316
Stuart Rankin <Stuart@Norio4.local>
2019-02-25 12:52:48 +0900

    Create WrapperPacket.swift
    Place-holder for refactoring effort to reduce complexity of filter wrappers.

---
+++ BumpCamera/Filters/FilterWrappers/WrapperPacket.swift
@@ -0,0 +1,15 @@
+//
+//  WrapperPacket.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/25/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WrapperPacket
+{
+    
+}
