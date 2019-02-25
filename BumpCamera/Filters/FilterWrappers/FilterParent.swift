9697c70f13a25832f0336e075d93b1fcec0f3a9a
Stuart Rankin <Stuart@Norio4.local>
2019-02-25 12:53:39 +0900

    Update FilterParent.swift
    Extracted RunFilterResults and extension. Added initial function for filter wrapper refactoring.

--- BumpCamera/Filters/FilterWrappers/FilterParent.swift
+++ BumpCamera/Filters/FilterWrappers/FilterParent.swift
@@ -343,42 +343,11 @@ class FilterParent
         let Rotated = Image.oriented(CGImagePropertyOrientation.downMirrored)
         return Rotated
     }
-}
-
-/// Filter execution result types.
-///
-/// - Success: Returned on successful execution of a given filter. Result contains the final
-///            successful result of the operation.
-/// - Failure: Returned on a failed execution of a given filter. Failure(let Reason) will
-///            return the reason (in a string) why the filter failed.
-public enum RunFilterResults<T>
-{
-    case Success(Result: T)
-    case Failure(Reason: String)
-}
-
-/// Equatable extension for the RunFilterResults enum.
-extension RunFilterResults: Equatable
-{
-    /// Compares two RunFilterResults for equality.
-    ///
-    /// - Parameters:
-    ///   - lhs: Left-hand side value.
-    ///   - rhs: Right-hand side value.
-    /// - Returns: True if the two sides are equal, false if not. Associated values for
-    ///            enum cases are not taken into account.
-    public static func ==(lhs: RunFilterResults, rhs: RunFilterResults) -> Bool
+    
+    // MARK: General rendering functions.
+    
+    func Render() -> RunFilterResults<Bool>
     {
-        switch (lhs, rhs)
-        {
-        case (.Success, .Success):
-            return true
-            
-        case (.Failure, .Failure):
-            return true
-            
-        default:
-            return false
-        }
+        return .Failure(Reason: "Not implemented.")
     }
 }
