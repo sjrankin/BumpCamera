d2c55a75d394e423884621849201b2ffa79aa570
Stuart Rankin <Stuart@Norio4.local>
2019-02-26 14:03:08 +0900

    Update Versioning.swift
    Automatically updated.

--- BumpCamera/Versioning.swift
+++ BumpCamera/Versioning.swift
@@ -46,19 +46,19 @@ public class Versioning
     }
     
     /// Build number.
-    public static let Build: Int = 1027
+    public static let Build: Int = 1032
     
     /// Build increment.
     private static let BuildIncrement = 1
     
     /// Build ID.
-    public static let BuildID: String = "971F7DE6-5A5E-49CF-B7D4-016B816724FE"
+    public static let BuildID: String = "BB322B4B-DD37-4D9D-8114-D4D8B545F5C3"
     
     /// Build date.
-    public static let BuildDate: String = "25 February 2019"
+    public static let BuildDate: String = "26 February 2019"
     
     /// Build Time.
-    public static let BuildTime: String = "22:35"
+    public static let BuildTime: String = "13:57"
     
     /// Return a standard build string.
     ///
