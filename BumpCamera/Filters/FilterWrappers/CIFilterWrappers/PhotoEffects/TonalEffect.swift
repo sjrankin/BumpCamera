afe53bcdb1ebf52d49368cde0d1be291b4744919
Stuart Rankin <Stuart@Norio4.local>
2019-02-28 17:54:07 +0900

    Create TonalEffect.swift
    Swift wrapper around the CIPhotoEffectTonal filter.

---
+++ BumpCamera/Filters/FilterWrappers/CIFilterWrappers/PhotoEffects/TonalEffect.swift
@@ -0,0 +1,360 @@
+//
+//  TonalEffect.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/28/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreMedia
+import CoreVideo
+import CoreImage
+
+class TonalEffect: FilterParent, Renderer
+{
+    static let _ID: UUID = UUID(uuidString: "46068424-35f2-4b51-a240-327c3ca19f57")!
+    
+    func ID() -> UUID
+    {
+        return type(of: self)._ID
+    }
+    
+    static func ID() -> UUID
+    {
+        return _ID
+    }
+    
+    var InstanceID: UUID
+    {
+        return UUID()
+    }
+    
+    static func Title() -> String
+    {
+        return "Tonal Effect"
+    }
+    
+    func Title() -> String
+    {
+        return type(of: self).Title()
+    }
+    
+    var IconName: String = "Tonal"
+    
+    var Description: String = "Tonal"
+    
+    var Initialized = false
+    
+    private var PrimaryFilter: CIFilter? = nil
+    
+    private var SecondaryFilter: CIFilter? = nil
+    
+    private var Context: CIContext? = nil
+    
+    private var BufferPool: CVPixelBufferPool? = nil
+    
+    private var ColorSpace: CGColorSpace? = nil
+    
+    private(set) var OutputFormatDescription: CMFormatDescription? = nil
+    
+    private(set) var InputFormatDescription: CMFormatDescription? = nil
+    
+    func Initialize(With FormatDescription: CMFormatDescription, BufferCountHint: Int)
+    {
+        Reset("TonalEffect.Initialize")
+        (BufferPool, ColorSpace, OutputFormatDescription) = CreateBufferPool(From: FormatDescription, BufferCountHint: BufferCountHint)
+        if BufferPool == nil
+        {
+            return
+        }
+        InputFormatDescription = FormatDescription
+        Context = CIContext()
+        PrimaryFilter = CIFilter(name: "CIPhotoEffectTonal")
+        Initialized = true
+    }
+    
+    func Reset(_ CalledBy: String = "")
+    {
+        objc_sync_enter(AccessLock)
+        defer{objc_sync_exit(AccessLock)}
+        Context = nil
+        PrimaryFilter = nil
+        ColorSpace = nil
+        //print("Setting BufferPool to nil in Noir, called by: \(CalledBy)")
+        BufferPool = nil
+        OutputFormatDescription = nil
+        InputFormatDescription = nil
+        Initialized = false
+    }
+    
+    func Reset()
+    {
+        Reset("")
+    }
+    
+    var AccessLock = NSObject()
+    
+    func Render(PixelBuffer: CVPixelBuffer) -> CVPixelBuffer?
+    {
+        objc_sync_enter(AccessLock)
+        defer{objc_sync_exit(AccessLock)}
+        guard let PrimaryFilter = PrimaryFilter,
+            let Context = Context,
+            Initialized else
+        {
+            print("Filter not initialized.")
+            return nil
+        }
+        
+        let Start = CACurrentMediaTime()
+        let SourceImage = CIImage(cvImageBuffer: PixelBuffer)
+        PrimaryFilter.setDefaults()
+        PrimaryFilter.setValue(SourceImage, forKey: kCIInputImageKey)
+        
+        guard let FilteredImage = PrimaryFilter.value(forKey: kCIOutputImageKey) as? CIImage else
+        {
+            print("CIFilter failed to render image.")
+            return nil
+        }
+        
+        var PixBuf: CVPixelBuffer?
+        CVPixelBufferPoolCreatePixelBuffer(kCFAllocatorDefault, BufferPool!, &PixBuf)
+        guard let OutPixBuf = PixBuf else
+        {
+            print("Allocation failure in.")
+            return nil
+        }
+        
+        Context.render(FilteredImage, to: OutPixBuf, bounds: FilteredImage.extent, colorSpace: ColorSpace)
+        
+        LiveRenderTime = CACurrentMediaTime() - Start
+        ParameterManager.UpdateRenderAccumulator(NewValue: LiveRenderTime, ID: ID(), ForImage: false)
+        return OutPixBuf
+    }
+    
+    func InitializeForImage()
+    {
+    }
+    
+    func Render(Image: UIImage) -> UIImage?
+    {
+        if let CImage = CIImage(image: Image)
+        {
+            if let Result = Render(Image: CImage)
+            {
+                LastCIImage = Result
+                let Final = UIImage(ciImage: Result)
+                LastUIImage = Final
+                return Final
+            }
+            else
+            {
+                print("Error returned Render.")
+                return nil
+            }
+        }
+        else
+        {
+            print("Error converting UIImage to CIImage.")
+            return nil
+        }
+    }
+    
+    func Render(Image: CIImage) -> CIImage?
+    {
+        objc_sync_enter(AccessLock)
+        defer{objc_sync_exit(AccessLock)}
+        #if false
+        guard let PrimaryFilter = PrimaryFilter,
+            Initialized else
+        {
+            print("Filter not initialized.")
+            return nil
+        }
+        #endif
+        
+        let Start = CACurrentMediaTime()
+        PrimaryFilter = CIFilter(name: "CIPhotoEffectTonal")
+        PrimaryFilter?.setDefaults()
+        PrimaryFilter?.setValue(Image, forKey: kCIInputImageKey)
+        
+        if let Result = PrimaryFilter?.value(forKey: kCIOutputImageKey) as? CIImage
+        {
+            ImageRenderTime = CACurrentMediaTime() - Start
+            ParameterManager.UpdateRenderAccumulator(NewValue: ImageRenderTime, ID: ID(), ForImage: true)
+            return Result
+        }
+        return nil
+    }
+    
+    /// Returns the generated image. If the filter does not support generated images nil is returned.
+    ///
+    /// - Returns: Nil is always returned.
+    func Generate() -> CIImage?
+    {
+        return nil
+    }
+    
+    var LastUIImage: UIImage? = nil
+    var LastCIImage: CIImage? = nil
+    
+    func LastImageRendered(AsUIImage: Bool) -> Any?
+    {
+        if AsUIImage
+        {
+            return LastUIImage as Any?
+        }
+        else
+        {
+            return LastCIImage as Any?
+        }
+    }
+    
+    func DefaultFieldValue(Field: FilterManager.InputFields) -> (FilterManager.InputTypes, Any?)
+    {
+        switch Field
+        {
+        case .RenderImageCount:
+            return (.IntType, 0 as Any?)
+            
+        case .CumulativeImageRenderDuration:
+            return (.DoubleType, 0.0 as Any?)
+            
+        case .RenderLiveCount:
+            return (.IntType, 0 as Any?)
+            
+        case .CumulativeLiveRenderDuration:
+            return (.DoubleType, 0.0 as Any?)
+            
+        default:
+            fatalError("Unexpected field \(Field) encountered.")
+        }
+    }
+    
+    func SupportedFields() -> [FilterManager.InputFields]
+    {
+        return type(of: self).SupportedFields()
+    }
+    
+    public static func SupportedFields() -> [FilterManager.InputFields]
+    {
+        var Fields = [FilterManager.InputFields]()
+        Fields.append(.RenderImageCount)
+        Fields.append(.CumulativeImageRenderDuration)
+        Fields.append(.RenderLiveCount)
+        Fields.append(.CumulativeLiveRenderDuration)
+        return Fields
+    }
+    
+    func SettingsStoryboard() -> String?
+    {
+        return type(of: self).SettingsStoryboard()
+    }
+    
+    public static func SettingsStoryboard() -> String?
+    {
+        return "NoParametersSettingsUI"
+    }
+    
+    func IsSlow() -> Bool
+    {
+        return false
+    }
+    
+    public static func FilterTarget() -> [FilterTargets]
+    {
+        return [.LiveView, .Video, .Still]
+    }
+    
+    func FilterTarget() -> [FilterTargets]
+    {
+        return type(of: self).FilterTarget()
+    }
+    
+    private var ImageRenderTime: Double = 0.0
+    private var LiveRenderTime: Double = 0.0
+    
+    /// Return the rendering time for the most recent image or live view render. Optionally reset the saved render
+    /// time. Render time is from start of function call to return, not just kernel/filter time. One data point isn't
+    /// terribly useful so be sure to collect several hundred in different environmental conditions to get a good idea
+    /// of the actual rendering time.
+    ///
+    /// - Parameters:
+    ///   - ForImage: If true, return the render time for the last image rendered. If false, return the render time
+    ///               for the last live view frame rendered.
+    ///   - Reset: If true, the render time is reset to 0.0 for the type of data being returned.
+    /// - Returns: The number of seconds it took to render the for the type of data specified by ForImage.
+    public func RenderTime(ForImage: Bool, Reset: Bool = false) -> Double
+    {
+        let Final = ForImage ? ImageRenderTime : LiveRenderTime
+        if Reset
+        {
+            if ForImage
+            {
+                ParameterManager.SetField(To: ID(), Field: .RenderImageCount, Value: 0)
+                ParameterManager.SetField(To: ID(), Field: .CumulativeImageRenderDuration, Value: 0.0)
+            }
+            else
+            {
+                ParameterManager.SetField(To: ID(), Field: .RenderLiveCount, Value: 0)
+                ParameterManager.SetField(To: ID(), Field: .CumulativeLiveRenderDuration, Value: 0.0)
+            }
+        }
+        return Final
+    }
+    
+    /// Returns a list of strings intended to be used as key words in Exif data.
+    ///
+    /// - Returns: List of key words associated with the filter, including setting values.
+    func ExifKeyWords() -> [String]
+    {
+        var Keywords = [String]()
+        Keywords.append("Filter: CIFilterEffectTonal")
+        Keywords.append("FilterType: CIFilter")
+        Keywords.append("FilterID: \(ID().uuidString)")
+        Keywords.append("Grayscale")
+        return Keywords
+    }
+    
+    /// Returns a dictionary of Exif key-value pairs. Intended for use for inclusion in image Exif data.
+    ///
+    /// - Returns: Dictionary of key-value data for top-level Exif data.
+    func ExifKeyValues() -> [String: String]
+    {
+        return [String: String]()
+    }
+    
+    var FilterKernel: FilterManager.FilterKernelTypes
+    {
+        get
+        {
+            return type(of: self).FilterKernel
+        }
+    }
+    
+    static var FilterKernel: FilterManager.FilterKernelTypes
+    {
+        get
+        {
+            return FilterManager.FilterKernelTypes.CIFilter
+        }
+    }
+    
+    /// Describes the available ports for the filter. Static version.
+    ///
+    /// - Returns: Array of ports.
+    static func Ports() -> [FilterPorts]
+    {
+        return [FilterPorts.Input, FilterPorts.Output]
+    }
+    
+    /// Describes the available ports for the filter.
+    ///
+    /// - Returns: Array of ports.
+    func Ports() -> [FilterPorts]
+    {
+        return type(of: self).Ports()
+    }
+}
