a9152f443e1735f8953d0ea7b6444cf7221327cc
Stuart Rankin <Stuart@Norio4.local>
2019-02-28 22:56:15 +0900

    Create CheckerboardParameters.swift
    Parameter block used to send instructions on how to draw a checkerboard pattern in the metal kernel.

---
+++ BumpCamera/Filters/BumpyKernels/Generators/Checkerboard/CheckerboardParameters.swift
@@ -0,0 +1,19 @@
+//
+//  CheckerboardParameters.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 2/28/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import simd
+
+struct CheckerboardParameters
+{
+    let Q1Color: simd_float4
+    let Q2Color: simd_float4
+    let Q3Color: simd_float4
+    let Q4Color: simd_float4
+    let BlockSize: simd_uint1
+}
