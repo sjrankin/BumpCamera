50ec5a39f7f00c60a89758f23dea6d23531e4df6
Stuart Rankin <Stuart@Norio4.local>
2019-03-05 23:19:35 +0900

    Create ChannelManglerParameters.swift
    Parameter buffer that tells the channel mangler how to mangle channels.

---
+++ BumpCamera/Filters/BumpyKernels/ChannelMangler/ChannelManglerParameters.swift
@@ -0,0 +1,15 @@
+//
+//  ChannelManglerParameters.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 3/5/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import simd
+
+struct ChannelManglerParameters
+{
+    let Action: simd_uint1
+}
