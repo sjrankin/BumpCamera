91966945a7fbf53a6b378373b912b7e7be4b778d
Stuart Rankin <Stuart@Norio4.local>
2019-03-05 23:20:18 +0900

    Create ImageScanParameters.swift
    Parameter block that tells the ImageScan kernel how to function.

---
+++ BumpCamera/Filters/BumpyKernels/ImageScan/ImageScanParameters.swift
@@ -0,0 +1,33 @@
+//
+//  ImageScanParameters.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 3/5/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import simd
+
+struct ImageScanParameters
+{
+    let Action: simd_uint1
+}
+
+/// Types of actions the ImageScan kernel can take.
+///
+/// - NOP: No operation. Nothing happens.
+/// - CumulativeRGBChannelValues: Accumulate the total number and value of red, green, blue, and alpha channels.
+/// - CumulativeHSBChannelValues: Accumulate the total number and value of hue, saturation, brightness, and alpha channels.
+/// - CumulativeCMYKChannelValues: Accumulate the total number and value of cyan, magenta, yellow, black, and alpha channels.
+/// - BrightestPixels: Find the locations of the brightest pixels near the center of each border
+/// - DarkestPixels: Find the location of the darkest pixels near the center of each border.
+enum ImageScanActions: Int
+{
+    case NOP = 0
+    case CumulativeRGBChannelValues = 1
+    case CumulativeHSBChannelValues = 2
+    case CumulativeCMYKChannelValues = 3
+    case BrightestPixels = 4
+    case DarkestPixels = 5
+}
