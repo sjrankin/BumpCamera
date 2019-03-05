ca4dffe628ed00ec428489e5bd2431b6e06e6ae1
Stuart Rankin <Stuart@Norio4.local>
2019-03-05 23:25:17 +0900

    Update DebugSettingsCode.swift
    Added support for enabling the activity log and deleting the debug directory.

--- BumpCamera/SettingsUI/DebugSettings/DebugSettingsCode.swift
+++ BumpCamera/SettingsUI/DebugSettings/DebugSettingsCode.swift
@@ -17,6 +17,8 @@ class DebugSettingsCode: UITableViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        ActivityLogSwitch.isOn = _Settings.bool(forKey: "UseActivityLog")
         FramerateSwitch.isOn = _Settings.bool(forKey: "ShowFramerateOverlay")
         IgnorePreviousCrashSwitch.isOn = _Settings.bool(forKey: "IgnorePriorCrashes")
         var LastBadFilter = (UIApplication.shared.delegate as? AppDelegate)?.CrashedFilterName
@@ -27,6 +29,13 @@ class DebugSettingsCode: UITableViewController
         LastCrashedFilterName.text = LastBadFilter
         self.tableView.tableFooterView = UIView()
     }
+    
+    @IBAction func HandleActivityLogChanged(_ sender: Any)
+    {
+        _Settings.set(ActivityLogSwitch.isOn, forKey: "UseActivityLog")
+    }
+    
+    @IBOutlet weak var ActivityLogSwitch: UISwitch!
     
     @IBAction func HandleFramerateChanged(_ sender: Any)
     {
@@ -48,6 +57,15 @@ class DebugSettingsCode: UITableViewController
     @IBOutlet weak var IgnorePreviousCrashSwitch: UISwitch!
     
     @IBOutlet weak var LastCrashedFilterName: UILabel!
+    
+    @IBAction func HandleClearDebugDirectoryButtonPressed(_ sender: Any)
+    {
+        FileHandler.ClearDirectory(FileHandler.DebugDirectory)
+        let Alert = UIAlertController(title: "Completed",
+                                      message: "The directory \(FileHandler.DebugDirectory) was cleared.",
+                                      preferredStyle: UIAlertController.Style.alert)
+        Alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+    }
     
     #endif
 }
