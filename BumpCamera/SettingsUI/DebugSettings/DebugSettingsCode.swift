52d8b5f43473f347f02be171f23c16f5522260e6
Stuart Rankin <Stuart@Norio4.local>
2019-03-09 18:12:20 +0900

    Update DebugSettingsCode.swift
    Added code to control how the heart beat indicator looks on the main UI.

--- BumpCamera/SettingsUI/DebugSettings/DebugSettingsCode.swift
+++ BumpCamera/SettingsUI/DebugSettings/DebugSettingsCode.swift
@@ -18,6 +18,10 @@ class DebugSettingsCode: UITableViewController
     {
         super.viewDidLoad()
         
+        ShowHeartBeatSwitch.isOn = _Settings.bool(forKey: "ShowHeartBeatIndicator")
+        let HeartBeatRate = _Settings.double(forKey: "HeartRate")
+        HeartBeatSpeedSegments.selectedSegmentIndex = SegmentFromHeartBeat(HeartBeatRate)
+        HeartBeatSwitch.isOn = _Settings.bool(forKey: "EnableHeartBeat")
         ActivityLogSwitch.isOn = _Settings.bool(forKey: "UseActivityLog")
         FramerateSwitch.isOn = _Settings.bool(forKey: "ShowFramerateOverlay")
         IgnorePreviousCrashSwitch.isOn = _Settings.bool(forKey: "IgnorePriorCrashes")
@@ -27,7 +31,72 @@ class DebugSettingsCode: UITableViewController
             LastBadFilter = "not recorded"
         }
         LastCrashedFilterName.text = LastBadFilter
+        RunHeartBeat()
         self.tableView.tableFooterView = UIView()
+    }
+    
+    let HeartRates: [Double: Int] =
+    [
+        1.5 : 0,
+        1.0 : 1,
+        0.5 : 2,
+        0.25: 3,
+    ]
+    
+    func SegmentFromHeartBeat(_ Value: Double) -> Int
+    {
+        if let Seg = HeartRates[Value]
+        {
+            return Seg
+        }
+        return 1
+    }
+    
+    func HeartBeatRateFromSegment(_ SegIndex: Int) -> Double
+    {
+        for (Rate, Index) in HeartRates
+        {
+            if Index == SegIndex
+            {
+                return Rate
+            }
+        }
+        return 1.0
+    }
+    
+    func RunHeartBeat()
+    {
+        StopHeartBeat()
+        let HeartSeg = HeartBeatSpeedSegments.selectedSegmentIndex
+        let HeartRate = HeartBeatRateFromSegment(HeartSeg)
+        HeartTimer = Timer.scheduledTimer(timeInterval: HeartRate, target: self,
+                                          selector: #selector(UpdateHeartIndicator),
+                                          userInfo: nil, repeats: true)
+    }
+    
+    func StopHeartBeat()
+    {
+        if HeartTimer != nil
+        {
+            HeartTimer?.invalidate()
+            HeartTimer = nil
+        }
+    }
+    
+    var HeartTimer: Timer? = nil
+    var ShowingEmptyHeart = true
+    
+    @objc func UpdateHeartIndicator()
+    {
+        if ShowingEmptyHeart
+        {
+            HeartRateIndicator.image = UIImage(named: "FilledHeart")
+        }
+        else
+        {
+            HeartRateIndicator.image = UIImage(named: "EmptyHeart")
+        }
+        ShowingEmptyHeart = !ShowingEmptyHeart
     }
     
     @IBAction func HandleActivityLogChanged(_ sender: Any)
@@ -55,6 +124,41 @@ class DebugSettingsCode: UITableViewController
     }
     
     @IBOutlet weak var IgnorePreviousCrashSwitch: UISwitch!
+    
+    @IBAction func HandleHeartBeatSwitch(_ sender: Any)
+    {
+        _Settings.set(HeartBeatSwitch.isOn, forKey: "EnableHeartBeat")
+    }
+    
+    @IBOutlet weak var HeartBeatSwitch: UISwitch!
+    
+    @IBAction func HandleShowHeartBeatSwitchChanged(_ sender: Any)
+    {
+        _Settings.set(ShowHeartBeatSwitch.isOn, forKey: "ShowHeartBeatIndicator")
+        if ShowHeartBeatSwitch.isOn
+        {
+            RunHeartBeat()
+        }
+        else
+        {
+            StopHeartBeat()
+            HeartRateIndicator.image = nil
+        }
+    }
+    
+    @IBAction func HandleHeartBeatSpeedChanged(_ sender: Any)
+    {
+        if ShowHeartBeatSwitch.isOn
+        {
+            RunHeartBeat()
+        }
+    }
+    
+    @IBOutlet weak var HeartBeatSpeedSegments: UISegmentedControl!
+    
+    @IBOutlet weak var ShowHeartBeatSwitch: UISwitch!
+    
+    @IBOutlet weak var HeartRateIndicator: UIImageView!
     
     @IBOutlet weak var LastCrashedFilterName: UILabel!
     
