b69af7c2779926461a1afebbee521d2fcea6b8dd
Stuart Rankin <Stuart@Norio4.local>
2019-03-11 18:21:34 +0900

    Update GradientEditorUICode.swift
    Removed unneeded code for controls no longer present. Added tap to change orientation of gradient functionality. Added build-in iOS table editing. Added functionality to segue to gradient stop editor.

--- BumpCamera/Main/CommonUI/GradientEditor/GradientEditorUICode.swift
+++ BumpCamera/Main/CommonUI/GradientEditor/GradientEditorUICode.swift
@@ -23,16 +23,34 @@ class GradientEditorUICode: UIViewController, GradientPickerProtocol,
         super.viewDidLoad()
         
         OriginalGradient = CurrentGradient
+        GradientStopList = GradientParser.ParseGradient(CurrentGradient)
         GradientView.backgroundColor = UIColor.black
         GradientView.layer.borderColor = UIColor.black.cgColor
         GradientView.layer.borderWidth = 0.5
         GradientView.layer.cornerRadius = 5.0
         GradientStopTable.delegate = self
         GradientStopTable.dataSource = self
+        GradientStopTable.layer.borderColor = UIColor.black.cgColor
+        GradientStopTable.layer.borderWidth = 0.5
+        GradientStopTable.layer.cornerRadius = 5.0
+        GradientStopTable.reloadData()
+        let TapGesture = UITapGestureRecognizer(target: self, action: #selector(HandleSampleTap))
+        GradientView.addGestureRecognizer(TapGesture)
         ShowSample(WithGradient: CurrentGradient)
     }
     
-    var GradientStopList = [(UIColor, Double)]()
+    @objc func HandleSampleTap(TapGesture: UITapGestureRecognizer)
+    {
+        if TapGesture.state == .ended
+        {
+            IsVertical = !IsVertical
+            ShowSample(WithGradient: CurrentGradient)
+        }
+    }
+    
+    var IsVertical = true
+    
+    var GradientStopList = [(UIColor, CGFloat)]()
     
     func ShowSample(WithGradient: String)
     {
@@ -44,9 +62,10 @@ class GradientEditorUICode: UIViewController, GradientPickerProtocol,
         }
         else
         {
-            let IsVertical = GradientOrientation.selectedSegmentIndex == 0
-            let SampleGradient = GradientParser.CreateGradientImage(From: WithGradient, WithFrame: GradientView.frame, IsVertical: IsVertical, ReverseColors: false)
+            let SampleGradient = GradientParser.CreateGradientImage(From: WithGradient, WithFrame: GradientView.bounds,
+                                                                    IsVertical: IsVertical, ReverseColors: false)
             GradientView.image = SampleGradient
+            GradientStopList = GradientParser.ParseGradient(WithGradient)
         }
         GradientStopTable.reloadData()
     }
@@ -58,7 +77,18 @@ class GradientEditorUICode: UIViewController, GradientPickerProtocol,
     
     func EditedGradient(_ Edited: String?, Tag: Any?)
     {
-        //Not used in this class.
+        //From the color stop editor.
+        if let NewGradient = Edited
+        {
+            if let CallerTag = Tag as? String
+            {
+                if CallerTag == "FromColorStopEditor"
+                {
+                    CurrentGradient = NewGradient
+                    ShowSample(WithGradient: CurrentGradient)
+                }
+            }
+        }
     }
     
     func GradientToEdit(_ EditMe: String?, Tag: Any?)
@@ -76,6 +106,7 @@ class GradientEditorUICode: UIViewController, GradientPickerProtocol,
     
     @IBAction func HandleReverseGradientButton(_ sender: Any)
     {
+        CurrentGradient = GradientParser.ReverseColorLocations(CurrentGradient)
         ShowSample(WithGradient: CurrentGradient)
     }
     
@@ -93,10 +124,26 @@ class GradientEditorUICode: UIViewController, GradientPickerProtocol,
     
     @IBAction func HandleEditButton(_ sender: Any)
     {
+        GradientStopTable.setEditing(!GradientStopTable.isEditing, animated: true)
+        if GradientStopTable.isEditing
+        {
+            EditButton.title = "Done"
+        }
+        else
+        {
+            EditButton.title = "Edit"
+        }
+        AddGradientStopButton.isEnabled = !GradientStopTable.isEditing
+        ResetButton.isEnabled = !GradientStopTable.isEditing
+        ReverseColorButton.isEnabled = !GradientStopTable.isEditing
+        ClearButton.isEnabled = !GradientStopTable.isEditing
     }
     
     @IBAction func HandleAddButton(_ sender: Any)
     {
+        CurrentGradient = GradientParser.AddGradientStop(CurrentGradient, Color: UIColor.red, Location: 1.0)
+        ShowSample(WithGradient: CurrentGradient)
+        GradientStopTable.reloadData()
     }
     
     @IBAction func HandleOrientationChange(_ sender: Any)
@@ -125,12 +172,76 @@ class GradientEditorUICode: UIViewController, GradientPickerProtocol,
     {
         let (Color, Location) = GradientStopList[indexPath.row]
         let Cell = GradientStopCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "GradientCell")
-        Cell.SetData(StopColor: Color, StopLocation: Location)
+        Cell.SetData(StopColor: Color, StopLocation: Double(Location))
         return Cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return GradientStopCell.CellHeight
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool
+    {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath)
+    {
+        if let NewGradient = GradientParser.SwapGradientStops(CurrentGradient, Index1: sourceIndexPath.row,
+                                                              Index2: destinationIndexPath.row)
+        {
+            CurrentGradient = NewGradient
+            ShowSample(WithGradient: CurrentGradient)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == .delete
+        {
+            CurrentGradient = GradientParser.RemoveGradientStop(CurrentGradient, AtIndex: indexPath.row)!
+            GradientStopTable.reloadData()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        if let Cell = GradientStopTable.cellForRow(at: indexPath) as? GradientStopCell
+        {
+            let (Color, Location) = Cell.CellData()
+            ColorToEdit = Color
+            LocationToEdit = Location
+            performSegue(withIdentifier: "ToGradientStopEditor", sender: self)
+        }
+    }
+    
+    var ColorToEdit: UIColor = UIColor.black
+    var LocationToEdit: Double = 0.0
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        switch segue.identifier
+        {
+        case "ToGradientStopEditor":
+            if let Dest = segue.destination as? ColorStopEditorCode
+            {
+                Dest.ParentDelegate = self
+                Dest.GradientToEdit(CurrentGradient, Tag: "FromColorStopEditor")
+                Dest.SetStop(StopColor: ColorToEdit, StopLocation: LocationToEdit)
+            }
+            
+        default:
+            break
+        }
+        super.prepare(for: segue, sender: self)
+    }
+    
+    @IBOutlet weak var ClearButton: UIBarButtonItem!
+    @IBOutlet weak var ResetButton: UIBarButtonItem!
+    @IBOutlet weak var ReverseColorButton: UIBarButtonItem!
+    @IBOutlet weak var AddGradientStopButton: UIBarButtonItem!
+    @IBOutlet weak var EditButton: UIBarButtonItem!
     @IBOutlet weak var GradientStopTable: UITableView!
     @IBOutlet weak var GradientView: UIImageView!
-    @IBOutlet weak var GradientOrientation: UISegmentedControl!
-    @IBOutlet weak var ReverseGradientButton: UIButton!
 }
