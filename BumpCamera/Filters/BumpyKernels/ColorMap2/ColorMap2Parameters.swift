28d70caf9ef17e82a64df7265428fa60b27ec112
Stuart Rankin <Stuart@Norio4.local>
2019-03-11 14:18:40 +0900

    Create ColorMap2Parameters.swift
    Parameters that control how ColorMap2 functions.

---
+++ BumpCamera/Filters/BumpyKernels/ColorMap2/ColorMap2Parameters.swift
@@ -0,0 +1,16 @@
+//
+//  ColorMap2Parameters.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 3/10/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import simd
+
+struct ColorMapParameters
+{
+    let InvertGradientDirection: simd_bool
+    let InvertGradientValues: simd_bool
+}
