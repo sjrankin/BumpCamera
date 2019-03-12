3319323560ff5eea6c1ad19090382a1c91c5c04e
Stuart Rankin <Stuart@Norio4.local>
2019-03-12 21:47:25 +0900

    Update PredefinedColor.swift
    Added debug statement.

--- BumpCamera/Main/CommonUI/ColorPicker/Colors/PredefinedColor.swift
+++ BumpCamera/Main/CommonUI/ColorPicker/Colors/PredefinedColor.swift
@@ -133,6 +133,7 @@ class PredefinedColor
     {
         let (ThisR, ThisG, ThisB) = Utility.GetRGB(Color)
         let (ThatR, ThatG, ThatB) = Utility.GetRGB(OtherColor)
+        print("Searching for \(ThatR),\(ThatG),\(ThatB)")
         return ThisR == ThatR && ThisG == ThatG && ThisB == ThatB
     }
     
