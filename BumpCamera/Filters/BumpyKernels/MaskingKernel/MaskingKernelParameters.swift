3e872003ba4a87dd3a379810a54311d7e7808350
Stuart Rankin <Stuart@Norio4.local>
2019-03-13 18:31:38 +0900

    Create MaskingKernelParameters.swift
    Parameter block to control the masking kernel.

---
+++ BumpCamera/Filters/BumpyKernels/MaskingKernel/MaskingKernelParameters.swift
@@ -0,0 +1,15 @@
+//
+//  MaskingKernelParameters.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 3/13/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import simd
+
+struct MaskingKernelParameters
+{
+    let MaskColor: simd_float4
+}
