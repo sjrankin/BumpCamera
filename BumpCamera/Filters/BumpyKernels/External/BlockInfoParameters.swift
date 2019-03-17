552b07a36c0302ac9a4b7677f302e0e5be07f8f7
Stuart Rankin <Stuart@Norio4.local>
2019-03-17 22:32:06 +0900

    Update BlockInfoParameters.swift
    Added HighlightPixelBy.

--- BumpCamera/Filters/BumpyKernels/External/BlockInfoParameters.swift
+++ BumpCamera/Filters/BumpyKernels/External/BlockInfoParameters.swift
@@ -14,6 +14,7 @@ struct BlockInfoParameters
     let Width: simd_uint1
     let Height: simd_uint1
     let HighlightAction: simd_uint1
+    let HighlightPixelBy: simd_uint1
     let BrightnessHighlight: simd_uint1
     let HighlightColor: simd_float4
     let ColorDetermination: simd_uint1
