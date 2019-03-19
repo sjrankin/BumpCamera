223aa9b425334215e05cae5ee30170c8bffb39b5
Stuart Rankin <Stuart@Norio4.local>
2019-03-19 22:15:37 +0900

    Create BlockMeanProtocol.swift
    Working on implementation of block mean.

---
+++ BumpCamera/SettingsUI/FilterSettingUIs/Metal/Measuration/BlockMean/BlockMeanProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  BlockMeanProtocol.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 3/19/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol BlockMeanProtocol
+{
+    func SetMeanData(MeanData: [(Int, Int, UIColor, Int)])
+}
