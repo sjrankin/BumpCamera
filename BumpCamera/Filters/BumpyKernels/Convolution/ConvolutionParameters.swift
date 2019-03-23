871033e4cdb32d9ab520262e9c4df669446ca34c
Stuart Rankin <sjrankin@gmail.com>
2019-03-23 23:04:26 +0900

    Create ConvolutionParameters.swift
    Parameter block for the Metal convolution kernel.

---
+++ BumpCamera/Filters/BumpyKernels/Convolution/ConvolutionParameters.swift
@@ -0,0 +1,18 @@
+//
+//  ConvolutionParameters.swift
+//  BumpCamera
+//
+//  Created by Stuart Rankin on 3/23/19.
+//  Copyright © 2019 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import simd
+
+struct ConvolveParameters
+{
+    let Width: simd_int1
+    let Height: simd_int1
+    let KernelCenterX: simd_int1
+    let KernelCenterY: simd_int1
+}
